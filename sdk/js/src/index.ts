dd9388d696b308e592c1695e20ecbd780c152d1f
chungchantal <chantal.chung@devcycle.com>
2022-03-10 13:10:06 -0800

    [DVC-2450] update error msg from globalThis to global

--- sdk/js/src/index.ts
+++ sdk/js/src/index.ts
@@ -15,7 +15,7 @@ export const initialize = (environmentKey: string, user: UserParam, options?: DV
         throw new Error('Window is not defined, try initializing in a browser context. If running on React Native, initialize with the option reactNative: true')
     }
     if (options?.reactNative && !globalThis.DeviceInfo) {
-        throw new Error('DeviceInfo is not defined. Import react-native-device-info and set globalThis.DeviceInfo when running on React Native')
+        throw new Error('DeviceInfo is not defined. Import react-native-device-info and set global.DeviceInfo when running on React Native')
     }
     if (!environmentKey) {
         throw new Error('Missing environment key! Call initialize with a valid environment key')
