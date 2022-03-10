fdf1396c4c61396a7bb0f165b799c9ad71e3b693
Adam Wootton <adam@taplytics.com>
2022-03-10 10:15:30 -0500

    [DVC-2602] make bucketing publishable

--- sdk/nodejs-server-sdk/src/utils/userBucketingHelper.ts
+++ sdk/nodejs-server-sdk/src/utils/userBucketingHelper.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
 import { BucketedUserConfig, ConfigBody } from '@devcycle/types'
 // eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
-import { generateBucketedConfig } from '@devcycle/shared/bucketing'
+import { generateBucketedConfig } from '@devcycle/bucketing'
 import { DVCUser } from '../../types'
 
 export function bucketUserForConfig(user: DVCUser, config?: ConfigBody): BucketedUserConfig | undefined {
