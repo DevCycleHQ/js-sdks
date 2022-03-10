86b2aece361e321abaedc567c9199c592cc755f2
Adam Wootton <adam@taplytics.com>
2022-03-10 12:02:52 -0500

    [DVC-2594] convert example apps to use nx

--- examples/react/with-async-provider/src/DevCycleExample.tsx
+++ examples/react/with-async-provider/src/DevCycleExample.tsx
@@ -2,12 +2,12 @@ import { useVariable } from '@devcycle/devcycle-react-sdk'
 import React from 'react'
 
 export default function DevCycleExample() {
-  const variableKey = 'test'
-  const variable = useVariable(variableKey, false)
+    const variableKey = 'test'
+    const variable = useVariable(variableKey, false)
 
-  return (
-    <div>
-      <span> Your variable is {variable?.value ? 'ON' : 'OFF'} </span>
-    </div>
-  )
+    return (
+        <div>
+            <span> Your variable is {variable?.value ? 'ON' : 'OFF'} </span>
+        </div>
+    )
 }
