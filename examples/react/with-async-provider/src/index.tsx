6f22ad0ec77f8277b573cf6f5dc2f50efb7f36a9
Adam Wootton <adam@taplytics.com>
2022-03-10 12:02:52 -0500

    [DVC-2594] convert example apps to use nx

--- examples/react/with-async-provider/src/index.tsx
+++ examples/react/with-async-provider/src/index.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
 import { asyncWithDVCProvider } from '@devcycle/devcycle-react-sdk';
 
 (async () => {
-  const ENV_KEY = 'test_token'
-  const DVCProvider = await asyncWithDVCProvider({envKey: ENV_KEY })
-  ReactDOM.render(
-    <React.StrictMode>
-      <DVCProvider>
-        <App />
-      </DVCProvider>
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
+    const ENV_KEY = 'test_token'
+    const DVCProvider = await asyncWithDVCProvider({ envKey: ENV_KEY })
+    ReactDOM.render(
+        <React.StrictMode>
+            <DVCProvider>
+                <App />
+            </DVCProvider>
+        </React.StrictMode>,
+        document.getElementById('root')
+    )
 })()
 
