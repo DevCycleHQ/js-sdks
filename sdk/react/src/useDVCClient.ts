1e79e66033ffbd8d43a686e5f38cd3bfbd113938
Adam Wootton <adam@taplytics.com>
2022-03-16 12:57:44 -0400

    [DVC-2713] explicitly declare return type to prevent typing issues

--- sdk/react/src/useDVCClient.ts
+++ sdk/react/src/useDVCClient.ts
@@ -1,7 +1,8 @@
 import { useContext } from 'react'
 import context from './context'
+import { DVCClient } from '@devcycle/devcycle-js-sdk'
 
-export const useDVCClient = () => {
+export const useDVCClient = (): DVCClient | undefined => {
     const { client } = useContext(context)
 
     return client
