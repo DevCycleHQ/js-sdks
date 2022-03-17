91e07dde1f3df7a143c73b1c9067fe83d411f831
Adam Wootton <adam@taplytics.com>
2022-03-17 11:31:36 -0400

    [DVC-2719] safe publish script

---
+++ scripts/npm-safe-publish.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Check for the existence of the version that's about to be published on NPM. If it exists, do nothing
+# If it doesn't exist, check that we're on the main branch, the working directory is clean, and the current commit
+# is tagged with the requested version
+
+if [[ $# -eq 0 ]]; then
+  echo "Must specify the package to push/check."
+  exit 1
+fi
+
+PACKAGE=$1
+NPM_SHOW="$(npm show "$PACKAGE" version)"
+NPM_LS="$(npm ls "$PACKAGE" --json | jq -r '.version')"
+
+if [[ "$NPM_SHOW" != "$NPM_LS" ]]; then
+  echo "Versions are not the same, (Remote = $NPM_SHOW; Local = $NPM_LS). Checking for publish eligibility."
+
+  # check if we're on main branch
+  if [[ "$(git rev-parse --abbrev-ref HEAD)" != "main" ]]; then
+    echo "Not on main branch. Aborting."
+    exit 1
+  fi
+
+  # check if working directory is clean
+  if [[ -n "$(git status --porcelain)" ]]; then
+    echo "Working directory is not clean. Aborting."
+    exit 1
+  fi
+
+  # check if current commit is tagged with the requested version
+  if [[ -z "$(git tag --points-at HEAD "$PACKAGE@$NPM_LS")" ]]; then
+    echo "Current commit is not tagged with the requested version. Aborting."
+    exit 1
+  fi
+
+  npx npm publish
+else
+  echo "Versions are the same ($NPM_SHOW = $NPM_LS). Not pushing"
+fi
