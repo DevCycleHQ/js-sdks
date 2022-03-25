aab4725d99d26cfbad414df3f6ff2939ff7295f1
Jonathan Norris <jonathan@taplytics.com>
2022-03-25 11:30:52 -0400

    [DVC-2582] fixes from PR comments

--- lib/shared/bucketing-assembly-script/assembly-script-src/helpers/lodashHelpers.ts
+++ lib/shared/bucketing-assembly-script/assembly-script-src/helpers/lodashHelpers.ts
@@ -1,11 +1,15 @@
 import { RegExp } from 'assemblyscript-regex'
 
-export function find<T>(array: Array<T>, callbackfn: (value: T, index: i32, array: Array<T>) => bool): T | null {
+export function find<T>(array: Array<T> | null, callbackfn: (value: T, index: i32, array: Array<T>) => bool): T | null {
+    if (!array) return null
+
     const index = array.findIndex(callbackfn)
     return index >= 0 ? array[index] : null
 }
 
-export function findString(array: Array<string>, findStr: string): string | null {
+export function findString(array: Array<string> | null, findStr: string): string | null {
+    if (!array) return null
+
     for (let i = 0; i < array.length; i++) {
         const value = array[i]
         if (findStr.includes(value)) {
@@ -15,12 +19,12 @@ export function findString(array: Array<string>, findStr: string): string | null
     return null
 }
 
-export function first<T>(array: Array<T>): T | null {
-    return array.length > 0 ? array[0] : null
+export function first<T>(array: Array<T> | null): T | null {
+    return (array && array.length > 0) ? array[0] : null
 }
 
-export function last<T>(array: Array<T>): T | null {
-    return array.length > 0 ? array[array.length - 1] : null
+export function last<T>(array: Array<T> | null): T | null {
+    return (array && array.length > 0) ? array[array.length - 1] : null
 }
 
 export function includes(string: string | null, other: string): bool {
