83bbf9f8ff846c5f518833ff950fce1b6b783607
Jason Salaber <jcsalaber@hotmail.com>
2022-03-31 12:44:51 -0400

    chore: use useVariable internally in useVariableValue

--- sdk/react/src/useVariableValue.ts
+++ sdk/react/src/useVariableValue.ts
@@ -1,14 +1,9 @@
-import { useContext } from 'react'
+import { useVariable } from './'
 import context from './context'
 import type { DVCVariableValue } from '@devcycle/devcycle-js-sdk'
 
 export const useVariableValue = (key: string, defaultValue: DVCVariableValue): DVCVariableValue => {
-    const { client } = useContext(context)
-    if (client) {
-        return client.variable(key, defaultValue).value
-    } else {
-        return defaultValue
-    }
+    return useVariable(key, defaultValue).value
 }
 
 export default useVariableValue
