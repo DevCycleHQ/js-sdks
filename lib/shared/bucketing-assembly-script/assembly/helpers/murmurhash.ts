faa6d4aa249ab69c8fd1852fe6b4f06efd5b72c5
Jamie Sinn <jamie.sinn@taplytics.com>
2022-04-04 16:25:14 -0400

    Murmurhash testing working between AS and JS.

--- lib/shared/bucketing-assembly-script/assembly/helpers/murmurhash.ts
+++ lib/shared/bucketing-assembly-script/assembly/helpers/murmurhash.ts
@@ -1,12 +1,23 @@
+export function murmurhashV3_js(key: string, seed: u32): string {
+    return `${murmurhashV3(key, seed)}`;
+}
 
-export function murmurhashV3(key: string, seed: i32): i32 {
-    const keyBuffer = Uint8Array.wrap(String.UTF8.encode(key))
-    console.log(`murmurhashV3 keyBuffer: ${keyBuffer}`)
+export function murmurhashV3(key: string, seed: u32): u32 {
+    let keyBuffer = new Int32Array(key.length);
+    for (let i = 0; i < key.length; i++) {
+        let charCode = i32(key.charCodeAt(i))
+        if (charCode > 255) {
+            throw new Error('Unsupported character in key.');
+        }
+        keyBuffer[i] = charCode
+    }
 
-    let remainder: i32, bytes: i32, h1: i32, h1b: i32, c1: i32, c2: i32, k1: i32, i: i32;
+    let remainder: i32, bytes: i32, h1b: i32, c1: i32, c2: i32, k1: i32, i: i32;
 
-    remainder = keyBuffer.byteLength & 3;
-    bytes = keyBuffer.byteLength - remainder;
+    let h1: i32;
+
+    remainder = key.length & 3;
+    bytes = keyBuffer.length - remainder;
     h1 = seed;
     c1 = 0xcc9e2d51;
     c2 = 0x1b873593;
@@ -19,6 +30,7 @@ export function murmurhashV3(key: string, seed: i32): i32 {
             ((keyBuffer[++i] & 0xff) << 16) |
             ((keyBuffer[++i] & 0xff) << 24);
         ++i;
+
 
         k1 = ((((k1 & 0xffff) * c1) + ((((k1 >>> 16) * c1) & 0xffff) << 16))) & 0xffffffff;
         k1 = (k1 << 15) | (k1 >>> 17);
@@ -33,23 +45,30 @@ export function murmurhashV3(key: string, seed: i32): i32 {
     k1 = 0;
 
     switch (remainder) {
-        case 3: k1 ^= (keyBuffer[i + 2] & 0xff) << 16;
-        case 2: k1 ^= (keyBuffer[i + 1] & 0xff) << 8;
-        case 1: k1 ^= (keyBuffer[i] & 0xff);
-
+        case 3:
+            k1 ^= (keyBuffer[i + 2] & 0xff) << 16;
+        case 2:
+            k1 ^= (keyBuffer[i + 1] & 0xff) << 8;
+        case 1:
+            k1 ^= (keyBuffer[i] & 0xff);
             k1 = (((k1 & 0xffff) * c1) + ((((k1 >>> 16) * c1) & 0xffff) << 16)) & 0xffffffff;
             k1 = (k1 << 15) | (k1 >>> 17);
             k1 = (((k1 & 0xffff) * c2) + ((((k1 >>> 16) * c2) & 0xffff) << 16)) & 0xffffffff;
             h1 ^= k1;
     }
 
-    h1 ^= keyBuffer.byteLength;
+    h1 ^= keyBuffer.length;
+
 
     h1 ^= h1 >>> 16;
+
     h1 = (((h1 & 0xffff) * 0x85ebca6b) + ((((h1 >>> 16) * 0x85ebca6b) & 0xffff) << 16)) & 0xffffffff;
+
     h1 ^= h1 >>> 13;
+
     h1 = ((((h1 & 0xffff) * 0xc2b2ae35) + ((((h1 >>> 16) * 0xc2b2ae35) & 0xffff) << 16))) & 0xffffffff;
+
     h1 ^= h1 >>> 16;
 
-    return h1 >>> 0;
+    return h1 as u32;
 }
