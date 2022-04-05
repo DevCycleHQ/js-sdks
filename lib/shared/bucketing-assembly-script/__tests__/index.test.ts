61f0a168ea1765ea77eb963459e5c593e272857c
Jonathan Norris <jonathan@taplytics.com>
2022-04-05 11:55:44 -0400

    DVC-2734 enforce flat JSON Objects for customData

--- lib/shared/bucketing-assembly-script/__tests__/index.test.ts
+++ lib/shared/bucketing-assembly-script/__tests__/index.test.ts
@@ -47,4 +47,32 @@ describe('WASM test', () => {
             lastSeenDate: expect.any(String)
         }))
     })
+
+    it('should throw error if customData is not a flat JSON Obj', () => {
+        const userObj = {
+            user_id: '24601',
+            customData: {
+                "string": "val",
+                "num": [610, 2809],
+            },
+        }
+
+        expect(() => testDVCUserClass(JSON.stringify(userObj)))
+            .toThrow('DVCUser customData can\'t contain nested objects or arrays')
+    })
+
+    it('should throw error if privateCustomData is not a flat JSON Obj', () => {
+        const userObj = {
+            user_id: '24601',
+            privateCustomData: {
+                "key": "val",
+                "values": {
+                    "obj": true
+                }
+            }
+        }
+
+        expect(() => testDVCUserClass(JSON.stringify(userObj)))
+            .toThrow('DVCUser privateCustomData can\'t contain nested objects or arrays')
+    })
 })
