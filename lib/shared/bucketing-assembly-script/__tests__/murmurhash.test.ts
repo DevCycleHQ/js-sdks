613b6335cb3acc1a65742f1fac77bd7442e6273c
Jamie Sinn <jamie.sinn@taplytics.com>
2022-04-05 15:57:42 -0400

    AS murmurhash fixes.

--- lib/shared/bucketing-assembly-script/__tests__/murmurhash.test.ts
+++ lib/shared/bucketing-assembly-script/__tests__/murmurhash.test.ts
@@ -3,56 +3,30 @@ import murmurhash from 'murmurhash'
 import {testMM3Internal} from "../build/bucketing-lib.debug";
 
 function randstring(length: number) {
-    var result = '';
-    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+/.,<>?;:[]{}|';
-    var charactersLength = characters.length;
-    for (var i = 0; i < length; i++) {
+    let result = '';
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+/.,<>?;:[]{}|\'"';
+    let charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
         result += characters.charAt(Math.floor(Math.random() *
             charactersLength));
     }
     return result;
 }
 
-describe('Test testMurmurhashV3', () => {
-    it('should return the correct hash for simple strings with a fixed seed', () => {
-        // console.log(murmurhash.v3('Orange', 0))//1637794643
-        // console.log(murmurhash.v3('Orange', 0))//1637794643
-        // console.log(murmurhash.v3('some-long-ascii-string', 1000))//605439249
-        // console.log(murmurhash.v3('some-long-ascii-string?', 1000))//784910028
-        // console.log(murmurhash.v3('some-long-ascii-string!', 1000))//1791583627
-        // console.log(murmurhash.v3('some-long-ascii-string*', 1000))//1578136692
-        // console.log(murmurhash.v3('some-long-ascii-string@', 1000))//4263889436
-        //
-        console.log(`murmurhash internal test - should return 1 ${testMM3Internal('some-long-ascii-string@', 1000)}`)
-        console.log(testMurmurhashV3('some-long-ascii-string@', 1000))
-        // expect(testMurmurhashV3('Orange', 1)).toBe(murmurhash.v3('Orange', 1))
-        // expect(testMurmurhashV3('some-long-ascii-string', 1000)).toEqual(murmurhash.v3('some-long-ascii-string', 1000))
-        // expect(testMurmurhashV3('some-long-ascii-string?', 1000)).toEqual(murmurhash.v3('some-long-ascii-string?', 1000))
-        // expect(testMurmurhashV3('some-long-ascii-string!', 1000)).toEqual(murmurhash.v3('some-long-ascii-string!', 1000))
-        // expect(testMurmurhashV3('some-long-ascii-string*', 1000)).toEqual(murmurhash.v3('some-long-ascii-string*', 1000))
-        // expect(testMurmurhashV3('some-long-ascii-string@', 1000)).toEqual(murmurhash.v3('some-long-ascii-string@', 1000))
-    })
-
+describe('Test testMurmurhashV3 Assemblyscript implementation', () => {
     it('should return the correct hash for simple strings with a varying seed', () => {
-        for (let i = 1000; i < 2000; i+=2) {
-            console.log(i)
-            expect(testMurmurhashV3('some-long-ascii-string', i)).toEqual(murmurhash.v3('some-long-ascii-string', i))
-            expect(testMurmurhashV3('some-long-ascii-string?', i)).toEqual(murmurhash.v3('some-long-ascii-string?', i))
-            expect(testMurmurhashV3('some-long-ascii-string!', i)).toEqual(murmurhash.v3('some-long-ascii-string!', i))
-            expect(testMurmurhashV3('some-long-ascii-string*', i)).toEqual(murmurhash.v3('some-long-ascii-string*', i))
+        for (let i = 0; i < 2000; i++) {
+            expect(testMM3Internal('some-long-ascii-string', i)).toContain(`${murmurhash.v3('some-long-ascii-string', i)}`)
+            expect(testMM3Internal('some-long-ascii-string?', i)).toContain(`${murmurhash.v3('some-long-ascii-string?', i)}`)
+            expect(testMM3Internal('some-long-ascii-string!', i)).toContain(`${murmurhash.v3('some-long-ascii-string!', i)}`)
+            expect(testMM3Internal('some-long-ascii-string*', i)).toContain(`${murmurhash.v3('some-long-ascii-string*', i)}`)
         }
     })
-/*
-    it('should return the correct hash for random strings and a varying seed', () => {
-        for (let i = 1; i < 1000; i++) {
-            const key = randstring(16)M
-            const wasmMMH = testMurmurhashV3(key, i)
-            const jsMMH = murmurhash.v3(key, i)
-            console.log(key, wasmMMH, jsMMH)
-            expect(wasmMMH).toEqual(jsMMH)
-        }
-    })*/
-})
 
-///-111011011110000000111101011
-///11111000100100001111111000010101
+    it('should return the correct hash for a random string with a varying seed', () => {
+        for (let i = 0; i < 20000; i++) {
+            const testString = randstring(100)
+            expect(testMM3Internal(testString, i)).toContain(`${murmurhash.v3(testString, i)}`)
+        }
+    })
+})
--- lib/shared/bucketing-assembly-script/assembly/helpers/murmurhash.ts
+++ lib/shared/bucketing-assembly-script/assembly/helpers/murmurhash.ts
@@ -1,79 +1,8 @@
 export function testInternal(key: string, seed: u32): string {
-    if(key == "Orange" && seed == 0) return `${murmurhashV3(key, seed) == 1637794643} - ${murmurhashV3(key, seed)}`;
-    if(key == "some-long-ascii-string" && seed == 1000) return `${murmurhashV3(key, seed) == 605439249} - ${murmurhashV3(key, seed)}`;
-    if(key == "some-long-ascii-string@" && seed == 1000) return `${murmurhashV3(key, seed) == 4263889436} - ${murmurhashV3(key, seed)}`;
     return `${murmurhashV3(key, seed)}`;
 }
 
-//
-// function mm3cpp(key: string, seed: u32): u32 {
-//
-//     let keyBuffer = new Int32Array(key.length);
-//     for (let i = 0; i < key.length; i++) {
-//         let charCode = i32(key.charCodeAt(i))
-//         if (charCode > 255) {
-//             throw new Error("Unsupported character in key.");
-//         }
-//         keyBuffer[i] = charCode
-//     }
-//
-//     const len = keyBuffer.length
-//     const nblocks = keyBuffer.length - (len & 3)
-//     let h1 = seed;
-//
-//     const c1 = 0xcc9e2d51;
-//     const c2 = 0x1b873593;
-//
-//     //----------
-//     // body
-//
-//     for (let i = 0; i < nblocks; i++) {
-//         let k1 = keyBuffer[i];
-//
-//         k1 *= c1;
-//         k1 = k1 << 15;
-//         k1 *= c2;
-//
-//         h1 ^= k1;
-//         h1 = h1 << 13;
-//         h1 = h1 * 5 + 0xe6546b64;
-//     }
-//
-//     //----------
-//     // tail
-//
-//     let k1 = 0;
-//
-//     switch (len & 3) {
-//         case 3:
-//             k1 ^= keyBuffer[2] << 16;
-//         case 2:
-//             k1 ^= keyBuffer[1] << 8;
-//         case 1:
-//             k1 ^= keyBuffer[0];
-//             k1 *= c1;
-//             k1 = k1 << 15;
-//             k1 *= c2;
-//             h1 ^= k1;
-//             break;
-//     }
-//
-//     //----------
-//     // finalization
-//
-//     h1 ^= len;
-//
-//     h1 ^= h1 >> 16;
-//     h1 *= 0x85ebca6b;
-//     h1 ^= h1 >> 13;
-//     h1 *= 0xc2b2ae35;
-//     h1 ^= h1 >> 16;
-//
-//     return h1 as u32
-// }
-
-
-export function murmurhashV3(key: string, seed: i32): u32 {
+export function murmurhashV3(key: string, seed: u32): u32 {
     if (seed < 0) {
         throw new Error("Seed must be positive.")
     }
@@ -133,16 +62,16 @@ export function murmurhashV3(key: string, seed: i32): u32 {
 
     h1 ^= keyBuffer.length;
 
-    console.log(`wasmH1_0: ${h1}`);
+
     h1 ^= h1 >>> 16;
-    console.log(`wasmH1_1: ${h1}`);
+
     h1 = (((h1 & 0xffff) * 0x85ebca6b) + ((((h1 >>> 16) * 0x85ebca6b) & 0xffff) << 16)) & 0xffffffff;
 
     h1 ^= h1 >>> 13;
 
     h1 = ((((h1 & 0xffff) * 0xc2b2ae35) + ((((h1 >>> 16) * 0xc2b2ae35) & 0xffff) << 16))) & 0xffffffff;
-    console.log(`wasmH1_2: ${h1}`);
+
     h1 ^= h1 >>> 16;
-    console.log(`wasmH1_3: ${h1}`);
+
     return h1 as u32;
 }
