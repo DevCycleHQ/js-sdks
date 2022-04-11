05392caece92eb90070ea58c6f3626269dabb54a
cristinataplytics <64020039+cristinataplytics@users.noreply.github.com>
2022-04-11 15:20:10 -0400

    Added new Env keys and now we show the Boolean variable value based on the selected Variation
    Added new Env keys and now we show the Boolean variable value based on the selected Variation

--- examples/react/next-js-app/pages/index.tsx
+++ examples/react/next-js-app/pages/index.tsx
@@ -15,7 +15,7 @@ const Home: NextPage = () => {
     const variable = useVariable( variableKey, true)
     const variableString = useVariable( variableKeyString, 'pink')
     const variableNumber = useVariable( variableKeyNumber, 100)
-    const variableBoolean = useVariable( variableKeyBoolean, true)
+    const variableBoolean = useVariable( variableKeyBoolean, false)
     const variableJson = useVariable( 'json-key-string', {'jsonStringKey1':'json string value 1'})
 
 
@@ -40,21 +40,39 @@ const Home: NextPage = () => {
                 </p>
 
                 <div className={styles.description}>
-                    <span> Your default variable feature-release = {variable?.value ? 'ON' : 'OFF'} </span>
                     <div>
-                        Your variable variableString = {variableString.value}
+                        <span> Your defaulte variable feature-release selected Variation =  {variable?.value ? 'ON' : 'OFF'} </span>
                     </div>
                     <div>
-                        Your variable variableNumber = {variableNumber.value}
+                        <span> Your variable feature-release value = {JSON.stringify(variable.value)} </span>
                     </div>
                     <div>
-                        Your variable variableBoolean = {variableBoolean.value ? 'ON' : 'OFF'}
+                        <span> Your variable variableString = {variableString.value} </span>
                     </div>
                     <div>
-                        Your variable variableJson = {JSON.stringify(variableJson.value)}
+                        <span> Your variable variableNumber = {variableNumber.value} </span>
                     </div>
-                
+                    <div>
+                        <span> Your variable variableBoolean selected Variation = {variableBoolean.value ? 'OFF' : 'ON'} </span>
+                    </div>
+                    <div>
+                        <span> Your variable variableBoolean value = {JSON.stringify(variableBoolean.value)} </span>
+                    </div>
+                    <div>   
+                        <span> Your variable variableJson = {JSON.stringify(variableJson.value)} </span>
+                    </div>
                 </div>
+
+
+
+
+
+
+
+
+
+
+
 
 
 
