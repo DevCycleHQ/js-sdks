dd0ab72e2266b2236b050b6deefeeebb0eee059f
chungchantal <chantal.chung@devcycle.com>
2022-04-19 14:44:41 -0700

    fix: change initializeDVCClient to return client instead of promise

--- sdk/react/src/useDVCClient.ts
+++ sdk/react/src/useDVCClient.ts
@@ -2,7 +2,7 @@ import { useContext } from 'react'
 import context from './context'
 import { DVCClient } from '@devcycle/devcycle-js-sdk'
 
-export const useDVCClient = (): DVCClient | undefined => {
+export const useDVCClient = (): DVCClient => {
     const { client } = useContext(context)
 
     return client
