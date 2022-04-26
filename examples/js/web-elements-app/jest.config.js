e1ad2081226d5ea841ef17f68b14a146676e1b73
Jason Salaber <jcsalaber@hotmail.com>
2022-04-26 14:57:50 -0400

    chore: added web app using WebElements to example apps

---
+++ examples/js/web-elements-app/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'examples-js-web-elements-app',
+  preset: '../../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  transform: {
+    '^.+\\.[tj]s$': 'babel-jest'
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/examples/js/web-elements-app'
+};
