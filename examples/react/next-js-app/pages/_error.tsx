3ee915f8449b8abfedecec8aedc95e3e2903089c
Nikolas LeBlanc <nikolas.leblanc@gmail.com>
2022-05-01 12:14:18 -0400

    adds _error page to next example

---
+++ examples/react/next-js-app/pages/_error.tsx
@@ -0,0 +1,22 @@
+import { NextPageContext } from 'next'
+
+interface ErrorComponentProps {
+	statusCode?: number;
+}
+
+function ErrorComponent({ statusCode }: ErrorComponentProps): JSX.Element {
+    return (
+        <p>
+            {statusCode
+                ? `An error ${statusCode} occurred on server`
+                : 'An error occurred on client'}
+        </p>
+    )
+}
+
+ErrorComponent.getInitialProps = ({ res, err }: NextPageContext) => {
+    const statusCode = res ? res.statusCode : err ? err.statusCode : 404
+    return { statusCode }
+}
+
+export default ErrorComponent
