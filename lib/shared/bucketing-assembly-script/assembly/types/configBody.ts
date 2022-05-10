1075e3abaabeb1eab21bcf61849758c164fbf29c
Adam Wootton <adam@taplytics.com>
2022-05-10 11:04:58 -0400

    feat: add project settings to config

--- lib/shared/bucketing-assembly-script/assembly/types/configBody.ts
+++ lib/shared/bucketing-assembly-script/assembly/types/configBody.ts
@@ -12,12 +12,14 @@ export class PublicProject extends JSON.Value {
     _id: string
     key: string
     a0_organization: string
+    settings: JSON.Obj
 
     constructor(project: JSON.Obj) {
         super()
         this._id = getStringFromJSON(project, '_id')
         this.key = getStringFromJSON(project, 'key')
         this.a0_organization = getStringFromJSON(project, 'a0_organization')
+        this.settings = getJSONObjFromJSON(project, 'settings')
     }
 
     stringify(): string {
@@ -25,6 +27,7 @@ export class PublicProject extends JSON.Value {
         json.set('_id', this._id)
         json.set('key', this.key)
         json.set('a0_organization', this.a0_organization)
+        json.set('settings', this.settings)
 
         return json.stringify()
     }
