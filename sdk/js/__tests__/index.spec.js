8098ec80cbc66fcd1723920b001ff524ad53daa6
Kaushal Kapasi <kaushal.kapasi@taplytics.com>
2022-06-17 14:01:02 -0400

    update js sdk initialize method to throw an error for invalid options

--- sdk/js/__tests__/index.spec.js
+++ sdk/js/__tests__/index.spec.js
@@ -1,11 +1,13 @@
 import * as DVCClient from '../src'
-import { getConfigJson } from '../src/Request'
-import { mocked } from 'ts-jest/utils'
+import * as Request from '../src/Request'
 
 jest.mock('../src/Request')
-mocked(getConfigJson).mockImplementation(() => {
+jest.spyOn(Request, 'getConfigJson').mockImplementation(() => {
     return Promise.resolve({})
 })
+
+const missingKeyError = 'Missing environment key! Call initialize with a valid environment key'
+const invalidOptionsError = 'Invalid options! Call initialize with valid options'
 
 describe('initialize tests', () => {
     it('should return client when calling initialize', () => {
@@ -15,12 +17,18 @@ describe('initialize tests', () => {
     })
 
     it('should throw an error if environment key is not passed in initialize', () => {
-        expect(() => DVCClient.initialize('')).toThrow(expect.anything())
-        expect(() => DVCClient.initialize()).toThrow(expect.anything())
+        expect(() => DVCClient.initialize('')).toThrow(missingKeyError)
+        expect(() => DVCClient.initialize()).toThrow(missingKeyError)
     })
 
     it('should throw an error if user is not passed in initialize', () => {
         expect(() => DVCClient.initialize('YOUR_CLIENT_SIDE_ID')).toThrow(expect.anything())
+    })
+
+    it('should throw an error if invalid optioins are passed in initialize', () => {
+        const user = { user_id: 'user1' }
+        expect(() => DVCClient.initialize('YOUR_CLIENT_SIDE_ID', user, null)).toThrow(invalidOptionsError)
+        expect(() => DVCClient.initialize('YOUR_CLIENT_SIDE_ID', user, false)).toThrow(invalidOptionsError)
     })
 
     it('should flush when pagehide is triggered', () => {
