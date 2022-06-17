6db9d3c92fb2fc6933d703a4fe45cb3b94d676c5
Kaushal Kapasi <kaushal.kapasi@taplytics.com>
2022-06-17 11:24:24 -0400

    move logger types to their own file. update types index to export logger file

---
+++ lib/shared/types/src/logger.ts
@@ -0,0 +1,20 @@
+// DVC Logger Shared types, used by JS and NodeJS SDKs
+export interface DVCLogger {
+  error(message: string): void
+
+  warn(message: string): void
+
+  info(message: string): void
+
+  debug(message: string): void
+}
+
+export type DVCDefaultLogLevel = 'debug' | 'info' | 'warn' | 'error'
+
+export type DVCDefaultLoggerOptions = {
+  level?: DVCDefaultLogLevel
+
+  logWriter?: (message: string) => void
+}
+
+export type defaultLogger = (options?: DVCDefaultLoggerOptions) => DVCLogger
