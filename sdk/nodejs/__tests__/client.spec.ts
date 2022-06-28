50c683d19d1c9a3987c39e60143b09e4ffac1e62
Rabia Qureshi <rabia.q123@gmail.com>
2022-06-28 14:31:16 -0400

    Added test to ensure variableDefaulted event is sent when variable is not in bucketed config

--- sdk/nodejs/__tests__/client.spec.ts
+++ sdk/nodejs/__tests__/client.spec.ts
@@ -30,13 +30,20 @@ describe('variable', () => {
     })
     
     describe('variableDefaulted event', () => {
-        test('does not get sent if variable is in bucketed config',async () => {
-            const client = new DVCClient('token')
+        let client: DVCClient
 
+        beforeAll(async () => {
+            client = new DVCClient('token')
             await client.onClientInitialized()
             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
             // @ts-ignore
             client.eventQueue.queueAggregateEvent = jest.fn()
+        })
+        beforeEach(() => {
+            jest.clearAllMocks()
+        })
+    
+        it('does not get sent if variable is in bucketed config',async () => {
             client.variable(user, 'test-key', false)
 
             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
@@ -44,6 +51,17 @@ describe('variable', () => {
             expect(client.eventQueue.queueAggregateEvent)
                 .toBeCalledWith(expectedUser, 
                     { type: 'variableEvaluated', target: 'test-key' }, 
+                    { 'variables': { ['test-key']: true } }
+                )
+        })
+        it('gets sent if variable is not in bucketed config',async () => {
+            client.variable(user, 'test-key-not-in-config', false)
+
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            expect(client.eventQueue.queueAggregateEvent)
+                .toBeCalledWith(expectedUser, 
+                    { type: 'variableDefaulted', target: 'test-key-not-in-config' }, 
                     { 'variables': { ['test-key']: true } }
                 )
         })
