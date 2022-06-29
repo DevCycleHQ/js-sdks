cad2d3222c1529dff9dbf57ed4c30b92c203ca48
chungchantal <chantal.chung@devcycle.com>
2022-06-29 09:36:24 -0700

    fix: use pattern to block dist folders not under node modules

--- examples/react/react-native-app/metro.config.js
+++ examples/react/react-native-app/metro.config.js
@@ -21,6 +21,7 @@ module.exports = (async () => {
                 assetExts: assetExts.filter((ext) => ext !== 'svg'),
                 sourceExts: [...sourceExts, 'svg'],
                 resolverMainFields: ['sbmodern', 'browser', 'main'],
+                blockList: exclusionList([/^(?!.*node_modules).*\/dist\/.*/]),
             },
         },
         {
