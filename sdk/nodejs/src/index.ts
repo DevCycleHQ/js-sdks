135a5f1ec5a127bffbea2b5665c215dfb218a750
Kaushal Kapasi <kaushal.kapasi@taplytics.com>
2022-09-02 09:53:31 -0400

    update nodejs sdk to default to local bucketing when initialized without options

--- sdk/nodejs/src/index.ts
+++ sdk/nodejs/src/index.ts
@@ -8,14 +8,14 @@ export * from './types'
 type DVCOptionsCloudEnabled = DVCOptions & { enableCloudBucketing: true }
 type DVCOptionsLocalEnabled = DVCOptions & { enableCloudBucketing?: false }
 
-export function initialize(environmentKey: string, options: DVCOptionsLocalEnabled): DVCClient
+export function initialize(environmentKey: string, options?: DVCOptionsLocalEnabled): DVCClient
 export function initialize(environmentKey: string, options: DVCOptionsCloudEnabled): DVCCloudClient
-export function initialize(environmentKey: string, options: DVCOptions): DVCClient | DVCCloudClient {
+export function initialize(environmentKey: string, options?: DVCOptions): DVCClient | DVCCloudClient {
     if (!environmentKey) {
         throw new Error('Missing environment key! Call initialize with a valid environment key')
     }
 
-    if (options.enableCloudBucketing) {
+    if (options?.enableCloudBucketing) {
         return new DVCCloudClient(environmentKey, options)
     }
     return new DVCClient(environmentKey, options)
