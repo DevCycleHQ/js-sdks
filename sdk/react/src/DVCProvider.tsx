6821a8926ab416059dd37b9a22b29cf6fa4529e8
Adam Wootton <adam@taplytics.com>
2022-10-06 12:02:16 -0400

    fix: react now rerenders properly on variable change

--- sdk/react/src/DVCProvider.tsx
+++ sdk/react/src/DVCProvider.tsx
@@ -1,5 +1,5 @@
 import { ProviderConfig } from './types'
-import React, { ReactNode, useEffect, useState } from 'react'
+import React, { ReactNode, useEffect } from 'react'
 import initializeDVCClient from './initializeDVCClient'
 import { Provider } from './context'
 import { DVCClient } from '@devcycle/devcycle-js-sdk'
@@ -13,18 +13,13 @@ let client: DVCClient | undefined
 
 export default function DVCProvider(props: Props): React.ReactElement {
     const { envKey, user, options } = props.config
-    const [_, forceRerender] = useState({})
 
     if (!client) {
         client = initializeDVCClient(envKey, user, options)
-        client.subscribe('newVariables', () => {
-            forceRerender({})
-        })
     }
 
     useEffect(() => {
         return () => {
-            client?.unsubscribe('newVariables')
             client?.close()
             client = undefined
         }
--- sdk/react/src/useVariable.ts
+++ sdk/react/src/useVariable.ts
@@ -1,22 +1,14 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import context from './context'
 import type { DVCVariable, DVCVariableValue } from '@devcycle/devcycle-js-sdk'
 
 export const useVariable = (key: string, defaultValue: DVCVariableValue): DVCVariable => {
     const dvcContext = useContext(context)
+    const [_, forceRerender] = useState({})
 
     if (dvcContext === undefined) throw new Error('useVariable must be used within DVCProvider')
 
-    return dvcContext.client ? dvcContext.client.variable(key, defaultValue) : {
-        value: defaultValue,
-        defaultValue: defaultValue,
-        isDefaulted: true,
-        // TODO fix this when variable hook rerendering is finished
-        onUpdate: (() => {
-            // no-op
-        }) as () => DVCVariable,
-        key
-    }
+    return dvcContext.client.variable(key, defaultValue).onUpdate(() => forceRerender({}))
 }
 
 export default useVariable
