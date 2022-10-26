52b80882ec1cd314e7a00480f4e73aaa9feb3de1
Jonathan Norris <jonathan@taplytics.com>
2022-10-26 12:05:12 -0400

    TAP-8702 report WASM memory usage, allow WebAssembly.MemoryDescriptor to be set

--- sdk/nodejs/src/bucketing.ts
+++ sdk/nodejs/src/bucketing.ts
@@ -1,8 +1,37 @@
 import { instantiate, Exports } from '@devcycle/bucketing-assembly-script'
+import { DVCLogger, DVCReporter } from '@devcycle/types'
+import { DVCOptions } from './types'
 
 let Bucketing: Exports | null
-export const importBucketingLib = async (): Promise<void> => {
+
+export const importBucketingLib = async (
+    { logger, options }:
+    { logger?: DVCLogger, options?: DVCOptions } = {}
+): Promise<void> => {
     Bucketing = await instantiate()
+    startTrackingMemoryUsage(Bucketing, logger, options?.reporter)
+}
+
+export const startTrackingMemoryUsage = (
+    Bucketing: Exports,
+    logger?: DVCLogger,
+    reporter?: DVCReporter,
+    interval: number = 30 * 1000
+): void => {
+    if (!reporter) return
+    trackMemoryUsage(Bucketing, reporter, logger)
+    setInterval(() => trackMemoryUsage(Bucketing, reporter, logger), interval)
+}
+
+export const trackMemoryUsage = (
+    Bucketing: Exports,
+    reporter: DVCReporter,
+    logger?: DVCLogger,
+): void => {
+    if (!reporter) return
+    const memoryUsageMB = Bucketing.memory.buffer.byteLength / 1e6
+    logger?.debug(`WASM memory usage: ${memoryUsageMB} MB`)
+    reporter.reportMetric('wasmMemoryMB', memoryUsageMB, {})
 }
 
 export const getBucketingLib = (): Exports => {
