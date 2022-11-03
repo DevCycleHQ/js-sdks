85557c1d2a06441e0a6ecfd6c8c6bfe3dd0462c8
Jason Salaber <jcsalaber@hotmail.com>
2022-11-03 10:15:00 -0400

    chore: use withPlugins to include nx plugin with transpile modules

--- examples/react/next-js-app/next.config.js
+++ examples/react/next-js-app/next.config.js
@@ -1,5 +1,23 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
+const withNx = require('@nrwl/next/plugins/with-nx')
 const withTM = require('next-transpile-modules')(['@devcycle/devcycle-js-sdk', '@devcycle/devcycle-react-sdk'])
+const withPlugins = require('next-compose-plugins')
 
-module.exports = withTM({})
+/**
+ * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
+ **/
+const nextConfig = {
+    nx: {
+    // Set this to true if you would like to to use SVGR
+    // See: https://github.com/gregberge/svgr
+        svgr: false,
+    },
+}
+
+const plugins = [ 
+    [withNx]
+]
+
+
+module.exports = withTM(withPlugins([...plugins], nextConfig))
 
