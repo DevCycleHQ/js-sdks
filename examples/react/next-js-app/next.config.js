d0f43297bb1c2f1fb7200a63706f8c527217adc5
Jason Salaber <jcsalaber@hotmail.com>
2022-11-03 10:12:20 -0400

    fix: use withTM to transpile modules that are used locally

--- examples/react/next-js-app/next.config.js
+++ examples/react/next-js-app/next.config.js
@@ -1,16 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withNx = require('@nrwl/next/plugins/with-nx')
+const withTM = require('next-transpile-modules')(['@devcycle/devcycle-js-sdk', '@devcycle/devcycle-react-sdk'])
 
-/**
- * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
- **/
-const nextConfig = {
-    nx: {
-    // Set this to true if you would like to to use SVGR
-    // See: https://github.com/gregberge/svgr
-        svgr: false,
-    },
-}
-
-module.exports = withNx(nextConfig)
+module.exports = withTM({})
 
