7ef11c24a5c3107870676ce2b1abe1227fa25d6b
Joli Lui <joli.lui@taplytics.com>
2022-12-05 15:21:36 -0500

    add config cache related methods

--- sdk/js/src/CacheStore.ts
+++ sdk/js/src/CacheStore.ts
@@ -1,10 +1,17 @@
 import { BucketedUserConfig, DVCLogger } from '@devcycle/types'
+import _ from 'lodash'
 import { DVCPopulatedUser } from './User'
 
 export const StoreKey = {
     Config: 'dvc:config',
     User: 'dvc:user',
-    AnonUserId: 'dvc:anonymous_user_id'
+    AnonUserId: 'dvc:anonymous_user_id',
+    AnonymousConfig: 'dvc:anonymous_config',
+    AnonymousConfigUserId: 'dvc:anonymous_config.user_id',
+    AnonymousConfigDate: 'dvc:anonymous_config.fetch_date',
+    IdentifiedConfig: 'dvc:identified_config',
+    IdentifiedConfigUserId: 'dvc:identified_config.user_id',
+    IdentifiedConfigDate: 'dvc:identified_config.fetch_date'
 }
 
 export abstract class CacheStore {
@@ -24,17 +31,31 @@ export abstract class CacheStore {
         return this.store?.getItem(storeKey)
     }
 
+    private getConfigStoreKey(user: DVCPopulatedUser) {
+        return user.isAnonymous ? StoreKey.AnonymousConfig : StoreKey.IdentifiedConfig
+    }
+
+    private getUserIdStoreKey(user: DVCPopulatedUser) {
+        return user.isAnonymous ? StoreKey.AnonUserId : StoreKey.IdentifiedConfigUserId
+    }
+
+    private getConfigFetchDateKey(user: DVCPopulatedUser) {
+        return user.isAnonymous ? StoreKey.AnonymousConfigDate : StoreKey.IdentifiedConfigDate
+    }
+
     remove(storeKey: string): void {
         this.store?.removeItem(storeKey)
     }
 
-    saveConfig(data: BucketedUserConfig): void {
-        this.save(StoreKey.Config, data)
+    saveConfig(data: BucketedUserConfig, user: DVCPopulatedUser): void {
+        const configKey = this.getConfigStoreKey(user)
+        this.save(configKey, data)
         this.logger?.info('Successfully saved config to local storage')
     }
 
-    loadConfig(): BucketedUserConfig | null | undefined {
-        const config = this.load(StoreKey.Config)
+    loadConfig(user: DVCPopulatedUser): BucketedUserConfig | null | undefined {
+        const configKey = this.getConfigStoreKey(user)
+        const config = this.load(configKey)
         return config ? JSON.parse(config) : config
     }
 
@@ -59,6 +80,31 @@ export abstract class CacheStore {
     loadAnonUserId(): string | null | undefined {
         const anonUserId = this.load(StoreKey.AnonUserId)
         return anonUserId ? JSON.parse(anonUserId) : anonUserId
+    }
+
+    saveConfigUserId(user: DVCPopulatedUser): void {
+        const { user_id: userId, isAnonymous } = user
+        const userIdKey = this.getUserIdStoreKey(user)
+        this.save(userIdKey, userId)
+        this.logger?.info(`Successfully saved ${isAnonymous ? 'anonymous': 'identified'} user id to local storage`)
+    }
+
+    loadConfigUserId(user: DVCPopulatedUser): string | null | undefined {
+        const userIdKey = this.getUserIdStoreKey(user)
+        const userId = this.load(userIdKey)
+        return userId ? JSON.parse(userId) : userId
+    }
+
+    saveConfigFetchDate(user: DVCPopulatedUser): void {
+        const fetchDateKey = this.getConfigFetchDateKey(user)
+        this.save(fetchDateKey, _.now())
+        this.logger?.info('Successfully saved config fetch date to local storage')
+    }
+
+    loadConfigFetchDate(user: DVCPopulatedUser): string | null | undefined {
+        const fetchDateKey = this.getConfigFetchDateKey(user)
+        const fetchDate = this.load(fetchDateKey)
+        return fetchDate ? JSON.parse(fetchDate) : fetchDate
     }
 }
 
