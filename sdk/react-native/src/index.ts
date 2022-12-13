7e0b322fdfe2900d64c602dcd03ca77297e2c67a
Kaushal Kapasi <kaushal.kapasi@taplytics.com>
2022-12-13 10:34:46 -0500

    [feat] react native sdk

---
+++ sdk/react-native/src/index.ts
@@ -0,0 +1,12 @@
+import { useDVCClient, useVariable, useVariableValue, useIsDVCInitialized } from '@devcycle/devcycle-react-sdk'
+import { withDVCProvider } from './withDVCProvider'
+import { DVCProvider } from './DVCProvider'
+
+export { 
+    DVCProvider, 
+    useDVCClient, 
+    useVariable, 
+    useVariableValue, 
+    withDVCProvider,
+    useIsDVCInitialized
+}
---
+++ sdk/react-native/src/withDVCProvider.ts
@@ -0,0 +1,7 @@
+import { withDVCProvider as ReactWithDVCProvider } from '@devcycle/devcycle-react-sdk'
+import { getReactNativeConfig } from './DVCProvider'
+
+export const withDVCProvider: typeof ReactWithDVCProvider = (config) => {
+    const reactNativeConfig = getReactNativeConfig(config)
+    return ReactWithDVCProvider(reactNativeConfig)
+}
