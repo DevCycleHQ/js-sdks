933eab304ac4c1786fa0eda90c59d39ddd147928
Kaushal Kapasi <kaushal.kapasi@taplytics.com>
2022-12-13 10:34:46 -0500

    [feat] react native sdk

---
+++ sdk/react-native/src/DVCProvider.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { DVCProvider as ReactDVCProvider } from '@devcycle/devcycle-react-sdk'
+import ReactNativeStore from './ReactNativeCacheStore'
+
+type configType = Parameters<typeof ReactDVCProvider>[0]
+
+export const DVCProvider: typeof ReactDVCProvider = (props) => {
+    const config = getReactNativeConfig(props.config)
+
+    return <ReactDVCProvider config={config}>{props.children}</ReactDVCProvider>
+}
+
+export const getReactNativeConfig = (config: configType['config']) => {
+    const rnConfig = {
+        ...config,
+        options: {
+            ...config.options,
+            reactNative: true
+        }
+    }
+    if (!config.options?.storage) {
+        rnConfig.options.storage = new ReactNativeStore()
+    }
+    return rnConfig
+}
+
---
+++ sdk/react-native/src/ReactNativeCacheStore.ts
@@ -0,0 +1,27 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { DVCStorage } from '@devcycle/devcycle-js-sdk'
+
+export class ReactNativeStore implements DVCStorage {
+    store: typeof AsyncStorage
+
+    constructor() {
+        this.store = AsyncStorage
+    }
+
+    save(storeKey: string, data: unknown): void {
+        this.store.setItem(storeKey, JSON.stringify(data))
+    }
+
+    load<T>(storeKey: string): Promise<T | null | undefined> {
+        return this.store.getItem(storeKey)
+            .then((item) => {
+                return (item ? JSON.parse(item) : null)
+            })
+    }
+
+    remove(storeKey: string): void {
+        this.store.removeItem(storeKey)
+    }
+}
+
+export default ReactNativeStore
