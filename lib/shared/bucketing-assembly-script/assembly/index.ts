6750fc03c06bebd606f7f31a75ad9960772bd7c3
Jonathan Norris <jonathan@taplytics.com>
2023-03-03 14:59:46 -0500

    add variableForUser() method to WASM code

--- lib/shared/bucketing-assembly-script/assembly/index.ts
+++ lib/shared/bucketing-assembly-script/assembly/index.ts
@@ -7,6 +7,7 @@ import { ConfigBody, DVCPopulatedUser, PlatformData } from './types'
 import { _clearPlatformData, _setPlatformData } from './managers/platformDataManager'
 import { _getConfigData, _setConfigData } from './managers/configDataManager'
 import { _getClientCustomData, _setClientCustomData } from './managers/clientCustomDataManager'
+import { queueVariableEvaluatedEvent } from './managers/eventQueueManager'
 
 export function generateBoundedHashesFromJSON(user_id: string, target_id: string): string {
     const boundedHash = _generateBoundedHashes(user_id, target_id)
@@ -22,6 +23,20 @@ export function generateBucketedConfigForUser(sdkKey: string, userStr: string): 
 
     const bucketedConfig = _generateBucketedConfig(config, user, _getClientCustomData(sdkKey))
     return bucketedConfig.stringify()
+}
+
+export function variableForUser(sdkKey: string, userStr: string, variableKey: string): string | null {
+    const config = _getConfigData(sdkKey)
+    const user = DVCPopulatedUser.fromJSONString(userStr)
+
+    const bucketedConfig = _generateBucketedConfig(config, user, _getClientCustomData(sdkKey))
+    const variable = bucketedConfig.variables.has(variableKey)
+        ? bucketedConfig.variables.get(variableKey)
+        : null
+
+    queueVariableEvaluatedEvent(sdkKey, bucketedConfig, variable, variableKey)
+
+    return variable ? variable.stringify() : null
 }
 
 export function setPlatformData(platformDataStr: string): void {
