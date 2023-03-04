e034ca73a66fde27cb15717a602dac2be104d60d
Adam Wootton <adam@taplytics.com>
2023-03-03 19:02:55 -0500

    chore: add mapping of variable key to feature that controls it for efficient access later

--- lib/shared/bucketing-assembly-script/assembly/managers/configDataManager.ts
+++ lib/shared/bucketing-assembly-script/assembly/managers/configDataManager.ts
@@ -1,9 +1,21 @@
-import { ConfigBody } from '../types'
+import { ConfigBody, Feature } from '../types'
 
 const _configData: Map<string, ConfigBody> = new Map()
 
+const _varKeyToFeatureMap: Map<string, Feature> = new Map()
+
 export function _setConfigData(sdkKey: string, configData: ConfigBody): void {
     _configData.set(sdkKey, configData)
+    for (let i = 0; i < configData.features.length; i++) {
+        const feature = configData.features[i]
+        for (let j = 0; j < feature.variations.length; j++) {
+            for (let k = 0; k < feature.variations[j].variables.length; k++) {
+                if (!_varKeyToFeatureMap.has(feature.variations[j].variables[k]._var)) {
+                    _varKeyToFeatureMap.set(feature.variations[j].variables[k]._var, feature)
+                }
+            }
+        }
+    }
 }
 
 export function _getConfigData(sdkKey: string): ConfigBody {
@@ -11,5 +23,13 @@ export function _getConfigData(sdkKey: string): ConfigBody {
         throw new Error('Config data is not set.')
     } else {
         return _configData.get(sdkKey)
+    }
+}
+
+export function _getFeatureForVarKey(varKey: string): Feature | null {
+    if (!_varKeyToFeatureMap.has(varKey)) {
+        return null
+    } else {
+        return _varKeyToFeatureMap.get(varKey)
     }
 }
