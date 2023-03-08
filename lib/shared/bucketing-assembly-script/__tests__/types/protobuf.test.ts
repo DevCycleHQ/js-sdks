1a7632f0e256371ad1bdb9a8ded4a562827b5183
Jonathan Norris <jonathan@taplytics.com>
2023-03-08 15:16:20 -0500

    update protobuf tests

--- lib/shared/bucketing-assembly-script/__tests__/types/protobuf.test.ts
+++ lib/shared/bucketing-assembly-script/__tests__/types/protobuf.test.ts
@@ -1,14 +1,14 @@
 import {
-    variableForUser_PB
+    variableForUser_PB,
+    VariableType
 } from '../bucketingImportHelper'
 import protobuf, { Type } from 'protobufjs'
 import path from 'path'
 import testData from '@devcycle/bucketing-test-data/json-data/testData.json'
 const { config } = testData
 import { initSDK } from '../setPlatformData'
-import {VariableType_PB} from "../../assembly/types/protobuf/as-generated/VariableType_PB";
 
-describe('protobuf tests', () => {
+describe('protobuf variable tests', () => {
     const sdkKey = 'sdkKey'
     let VariableForUserParams_PB: Type
     let SDKVariable_PB: Type
@@ -43,26 +43,22 @@ describe('protobuf tests', () => {
 
         const pbMsg = VariableForUserParams_PB.create(params)
         const buffer = VariableForUserParams_PB.encode(pbMsg).finish()
-        // const badBuffer = new Uint8Array([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
-        // const bufferWithGarbage = Buffer.concat([buffer, badBuffer])
         const resultBuffer = variableForUser_PB(buffer)
-        if (resultBuffer) {
-            const sdkVariable = SDKVariable_PB.decode(resultBuffer)
-            expect(sdkVariable).toEqual({
-                '_id': '615356f120ed334a6054564c',
-                'boolValue': false,
-                'doubleValue': 0,
-                'evalReason': {
-                    'isNull': true,
-                    'value': '',
-                },
-                'key': 'swagTest',
-                'stringValue': 'YEEEEOWZA',
-                'type': VariableType_PB.String,
-            })
-        } else {
-            throw new Error('resultBuffer is null')
-        }
+        expect(resultBuffer).not.toBeNull()
+
+        const sdkVariable = SDKVariable_PB.decode(resultBuffer!)
+        expect(sdkVariable).toEqual({
+            '_id': '615356f120ed334a6054564c',
+            'boolValue': false,
+            'doubleValue': 0,
+            'evalReason': {
+                'isNull': true,
+                'value': '',
+            },
+            'key': 'swagTest',
+            'stringValue': 'YEEEEOWZA',
+            'type': VariableType.String,
+        })
     })
 
 })
