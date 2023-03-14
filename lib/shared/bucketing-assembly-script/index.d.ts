dcbf53319ee0a8002f6b7185e251d44b6994cf26
Jonathan Norris <jonathan@taplytics.com>
2023-03-14 15:44:00 -0400

    add memory testing

--- lib/shared/bucketing-assembly-script/index.d.ts
+++ lib/shared/bucketing-assembly-script/index.d.ts
@@ -2,6 +2,7 @@ import { __AdaptedExports } from './build/bucketing-lib.release'
 
 export * as ProtobufTypes from './protobuf/compiled'
 
+export const getWASMFileData: () => Uint8Array
 export const instantiate: (debug: boolean = false) => Promise<typeof __AdaptedExports>
 
 export type Exports = typeof __AdaptedExports
--- sdk/nodejs/src/bucketing.ts
+++ sdk/nodejs/src/bucketing.ts
@@ -1,6 +1,7 @@
-import { instantiate, Exports } from '@devcycle/bucketing-assembly-script'
+import { instantiate, Exports, getWASMFileData } from '@devcycle/bucketing-assembly-script'
 import { DVCLogger, DVCReporter } from '@devcycle/types'
 import { DVCOptions } from './types'
+// import { analysis } from "as-heap-analyzer/dist/index.js";
 
 let Bucketing: Exports | null
 let InstantiatePromise: Promise<Exports> | null
@@ -13,7 +14,7 @@ export const importBucketingLib = async (
         await InstantiatePromise
         return
     }
-    InstantiatePromise = instantiate().then((exports) => {
+    InstantiatePromise = instantiate(true).then((exports) => {
         Bucketing = exports
         return Bucketing
     })
@@ -24,24 +25,69 @@ export const importBucketingLib = async (
 export const startTrackingMemoryUsage = (
     logger?: DVCLogger,
     reporter?: DVCReporter,
-    interval: number = 30 * 1000
+    interval: number = 10 * 1000
 ): void => {
     if (!reporter) return
-    trackMemoryUsage(reporter, logger)
-    setInterval(() => trackMemoryUsage(reporter, logger), interval)
+    setTimeout(() => {
+        trackMemoryUsage(reporter, logger)
+        setInterval(() => trackMemoryUsage(reporter, logger), interval)
+    }, interval)
 }
 
-export const trackMemoryUsage = (
+let firstMemoryUsage: Map<string, number> | null = null
+let lastMemoryUsage: Map<string, number> | null = null
+
+const diffMaps = (map1: Map<string, number>, map2: Map<string, number>) => {
+    const diff: Record<string, number> = {}
+    for (const key of map2.keys()) {
+        const value1 = map1.get(key)
+        const value2 = map2.get(key)
+
+        if (!value1 && value2) {
+            diff[key] = value2
+        } else if (value1 && value2) {
+            diff[key] = value2 - value1
+        }
+    }
+
+    const keys = Object.keys(diff).sort((a, b) => diff[b] - diff[a])
+    keys.filter((key) => diff[key] !== 0).forEach((key) => {
+        const value = diff[key]
+        console.log(`${key} use ${value} bytes`)
+    })
+
+    // console.log('Diff from first memory usage:')
+    // console.log(diff)
+}
+
+export const trackMemoryUsage = async (
     reporter: DVCReporter,
     logger?: DVCLogger,
-): void => {
+): Promise<void> => {
     if (!reporter) return
     if (!Bucketing) {
         throw new Error('Bucketing lib not initialized')
     }
+
     const memoryUsageMB = Bucketing.memory.buffer.byteLength / 1e6
     logger?.debug(`WASM memory usage: ${memoryUsageMB} MB`)
     reporter.reportMetric('wasmMemoryMB', memoryUsageMB, {})
+
+    const { analysis } = await import('as-heap-analyzer/dist/index.js')
+    const memoryUsage = analysis(new Uint8Array(Bucketing.memory.buffer), getWASMFileData())
+    if (!firstMemoryUsage) {
+        firstMemoryUsage = memoryUsage
+        lastMemoryUsage = memoryUsage
+    } else {
+        console.log('Diff from first memory usage:')
+        diffMaps(firstMemoryUsage, memoryUsage)
+        console.log('Diff from last memory usage:')
+        if (lastMemoryUsage) diffMaps(lastMemoryUsage, memoryUsage)
+        lastMemoryUsage = memoryUsage
+    }
+
+    // console.log(JSON.stringify(memoryUsage, replacer))
+    // memoryUsage.forEach((v, k) => console.log(`${k} use ${v} bytes`))
 }
 
 export const getBucketingLib = (): Exports => {
