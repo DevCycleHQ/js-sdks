f3e31c93152c1a37994bac51ea8f0f4ae9a23c0c
Jonathan Norris <jonathan@taplytics.com>
2023-03-14 14:50:44 -0400

    setup bench WASM in example app

--- examples/nodejs-local/typescript/src/main.ts
+++ examples/nodejs-local/typescript/src/main.ts
@@ -4,6 +4,7 @@ import { plainToInstance } from 'class-transformer'
 import { Query } from 'express-serve-static-core'
 import express from 'express'
 import bodyParser from 'body-parser'
+import { promisify } from 'util'
 
 const DVC_SERVER_SDK_KEY = process.env['DVC_SERVER_SDK_KEY'] || '<YOUR_DVC_SERVER_SDK_KEY>'
 
@@ -73,7 +74,59 @@ async function startDVC() {
     console.dir(features)
 }
 
-startDVC()
+
+async function benchDVC() {
+    if (!dvcClient) {
+        console.log('start bench')
+        dvcClient = await initialize(DVC_SERVER_SDK_KEY, {
+            logLevel: 'debug',
+            enableCloudBucketing: false,
+            disableAutomaticEventLogging: true,
+            reporter: {
+                reportMetric: (report) => {
+                    console.log(report)
+                },
+                reportFlushResults: (report) => {
+                    console.log(report)
+                }
+            }
+        }).onClientInitialized()
+    }
+
+    // console.log('wait 3 seconds')
+    await promisify(setTimeout)(500)
+    // console.log('finished waiting 0.5 seconds')
+
+    const user = {
+        user_id: 'node_sdk_test',
+        country: 'CA',
+        // customData: { 'customDataKey': 'customDataValue', num: 610, bool: false },
+        // privateCustomData: { 'customDataKey': 'customDataValue', num: 610, bool: false }
+    }
+    let variable
+    const time = performance.now()
+    const count = 50000
+
+    for (let i = 0; i < count; i++) {
+        variable = dvcClient.variable(user, 'v-key-25', false)
+    }
+
+    if (variable?.isDefaulted) {
+        throw new Error('Variable should not be defaulted')
+    } else {
+        const endTime = performance.now() - time
+        console.log(
+            `Variable 'v-key-25' value is ${variable?.value}, total: ${endTime}ms, per call: ${endTime / count}ms`
+        )
+    }
+    benchDVC()
+}
+
+if (process.env.DVC_BENCHMARK) {
+    benchDVC()
+} else {
+    startDVC()
+}
 
 app.get('/variables', (req: express.Request, res: express.Response) => {
     const user = validateUserFromQueryParams(req.query)
