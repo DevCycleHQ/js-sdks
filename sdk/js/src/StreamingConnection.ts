63544c306b5b98c4a0b6467f3d1853ed2e010d14
Kaushal Kapasi <kaushal.kapasi@taplytics.com>
2023-03-15 13:52:04 -0400

    DVC-6132 [fix]: update realtime connection onerror to always be warning logs. update config fetch error / error log to capture the response errors

--- sdk/js/src/StreamingConnection.ts
+++ sdk/js/src/StreamingConnection.ts
@@ -2,7 +2,6 @@ import { DVCLogger } from '@devcycle/types'
 
 export class StreamingConnection {
     private connection: EventSource
-    private hasSuccessfullyConnected: boolean = false
 
     constructor(
         private url: string,
@@ -17,15 +16,10 @@ export class StreamingConnection {
         this.connection.onmessage = (event) => {
             this.onMessage(event.data)
         }
-        this.connection.onerror = (err) => {
-            if (this.hasSuccessfullyConnected) {
-                this.logger.warn(`StreamingConnection error ${err}`)
-            } else {
-                this.logger.error('StreamingConnection error', err)
-            }
+        this.connection.onerror = () => {
+            this.logger.warn('StreamingConnection warning. Connection failed to establish.')
         }
         this.connection.onopen = () => {
-            this.hasSuccessfullyConnected = true
             this.logger.debug('StreamingConnection opened')
         }
     }
