5b55203b5a26b19600f22c0812d3dd221f1cdde0
Jonathan Norris <jonathan@taplytics.com>
2023-03-17 10:36:18 -0400

    DVC-6487 switch NodeJS SDK to use variableForUser() (#435)
    * switch NodeJS SDK to use variableForUser()
    
    * fix linting issue
    
    * fix test mock to fix VariableType enum

--- sdk/nodejs/src/utils/userBucketingHelper.ts
+++ sdk/nodejs/src/utils/userBucketingHelper.ts
@@ -1,4 +1,4 @@
-import { BucketedUserConfig } from '@devcycle/types'
+import { BucketedUserConfig, SDKVariable, VariableType } from '@devcycle/types'
 import { DVCPopulatedUser } from '../models/populatedUser'
 import { getBucketingLib } from '../bucketing'
 
@@ -6,4 +6,26 @@ export function bucketUserForConfig(user: DVCPopulatedUser, token: string): Buck
     return JSON.parse(
         getBucketingLib().generateBucketedConfigForUser(token, JSON.stringify(user))
     ) as BucketedUserConfig
+}
+
+export function getVariableTypeCode(type: VariableType): number {
+    const Bucketing = getBucketingLib()
+    switch (type) {
+        case VariableType.boolean:
+            return Bucketing.VariableType.Boolean
+        case VariableType.number:
+            return Bucketing.VariableType.Number
+        case VariableType.string:
+            return Bucketing.VariableType.String
+        case VariableType.json:
+            return Bucketing.VariableType.JSON
+        default:
+            throw new Error(`Unknown variable type: ${type}`)
+    }
+}
+
+export function variableForUser(token: string, usr: DVCPopulatedUser, key: string, type: number): SDKVariable | null {
+    const bucketedVariable = getBucketingLib().variableForUser(token, JSON.stringify(usr), key, type, true)
+    if (!bucketedVariable) return null
+    return JSON.parse(bucketedVariable) as SDKVariable
 }
