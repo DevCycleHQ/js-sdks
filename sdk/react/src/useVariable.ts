b6ec899a3c343bc8de5bd397f115824a89826764
Adam Wootton <adam@taplytics.com>
2023-04-28 11:40:12 -0400

    chore: revert hook types and export needed types for cli (#485)

--- sdk/react/src/useVariable.ts
+++ sdk/react/src/useVariable.ts
@@ -1,17 +1,17 @@
 import { useContext, useRef, useState } from 'react'
 import context from './context'
 import type { DVCVariable, DVCVariableValue } from '@devcycle/devcycle-js-sdk'
-import { VariableDefinitions } from '@devcycle/devcycle-js-sdk'
 
-export const useVariable = <Variables extends VariableDefinitions = VariableDefinitions,
-    K extends string & keyof Variables = string & keyof Variables,
-    T extends DVCVariableValue & Variables[K] = DVCVariableValue & Variables[K]
->(key: K, defaultValue: T): DVCVariable<T> => {
+export const useVariable = <T extends DVCVariableValue>(
+    key: string,
+    defaultValue: T
+): DVCVariable<T> => {
     const dvcContext = useContext(context)
     const [_, forceRerender] = useState({})
     const ref = useRef<DVCVariable<T>>()
 
-    if (dvcContext === undefined) throw new Error('useVariable must be used within DVCProvider')
+    if (dvcContext === undefined)
+        throw new Error('useVariable must be used within DVCProvider')
 
     if (!ref.current) {
         ref.current = dvcContext?.client.variable(key, defaultValue)
