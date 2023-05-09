0e4b2fae6cdf7f143e16b788939095e71f03d058
Nikolas LeBlanc <nikolas.leblanc@gmail.com>
2023-05-09 10:18:45 -0400

    fix gitignore to exclude class but allow java

---
+++ rhinotest.java
@@ -0,0 +1,20 @@
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class RhinoTest {
+    public static void main(String[] args) {
+        Context context = Context.enter();
+        try {
+            Scriptable scope = context.initStandardObjects();
+            FileReader scriptFile = new FileReader("dist/sdk/nodejs2/main.js");
+            context.evaluateReader(scope, scriptFile, "JavaScript", 1, null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            Context.exit();
+        }
+    }
+}
