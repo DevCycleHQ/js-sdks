190f487965dc8aafe39d89bc57952e83a43ef04b
Adam Wootton <adam@taplytics.com>
2023-06-08 13:51:22 -0400

    chore: ran migrations

--- lib/shared/bucketing-assembly-script/jest-bench.config.ts
+++ lib/shared/bucketing-assembly-script/jest-bench.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable */
 // import * as preset from '../../../jest.preset'
 const displayName = 'bucketing-lib-as-benchmark'
 
@@ -7,7 +8,7 @@ export default {
     globals: {
         'ts-jest': {
             tsconfig: '<rootDir>/tsconfig.spec.json',
-        }
+        },
     },
     testEnvironment: 'jest-bench/environment',
     testEnvironmentOptions: {
@@ -15,20 +16,15 @@ export default {
         testEnvironment: 'jest-environment-node',
         testEnvironmentOptions: {
             // specify any option for your environment
-        }
+        },
     },
-    transformIgnorePatterns: [
-        '<rootDir>/node_modules/(?!@assemblyscript/.*)'
-    ],
+    transformIgnorePatterns: ['<rootDir>/node_modules/(?!@assemblyscript/.*)'],
     transform: {
-        '^.+\\.[tj]sx?$': 'ts-jest'
+        '^.+\\.[tj]sx?$': 'ts-jest',
     },
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-    reporters: [
-        'default',
-        'jest-bench/reporter'
-    ],
+    reporters: ['default', 'jest-bench/reporter'],
     testMatch: undefined,
     // will pick up "*.bench.js" files or files in "__benchmarks__" folder.
-    testRegex: '(/__benchmarks__/.*|\\.bench)\\.(ts|tsx|js)$'
+    testRegex: '(/__benchmarks__/.*|\\.bench)\\.(ts|tsx|js)$',
 }
