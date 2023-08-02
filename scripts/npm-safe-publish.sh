b88c5771a5831d8003825a76ed75acc329135e30
Jonathan Norris <jonathan@taplytics.com>
2023-08-02 11:21:11 -0400

    fix: add 10 sec sleep to make npm publish script work with npm deprecate (#538)

--- scripts/npm-safe-publish.sh
+++ scripts/npm-safe-publish.sh
@@ -126,6 +126,8 @@ if [[ "$DEPRECATED_PACKAGE" != "" ]]; then
     echo "Publishing $DEPRECATED_PACKAGE@$NPM_LS to NPM."
     npm publish --otp=$OTP
 
+    sleep 10
+
     npm deprecate "$DEPRECATED_PACKAGE"@"*" "Package has been renamed to: $PACKAGE" --otp=$OTP
 
     # Restore the original package.json (trap will take care of this if the script exits prematurely)
