aa1c1e393d71277d8f58371bcf04113f97da3eb9
Nikolas LeBlanc <nikolas.leblanc@gmail.com>
2023-08-02 21:20:45 -0400

    feat: add react-native-expo pacakage

---
+++ sdk/react-native-expo/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+    displayName: 'react-native-lib',
+    preset: 'react-native',
+    resolver: '@nx/jest/plugins/resolver',
+    moduleFileExtensions: ['ts', 'js', 'html', 'tsx', 'jsx'],
+    setupFilesAfterEnv: ['<rootDir>/test-setup.ts'],
+    moduleNameMapper: {
+        '.svg': '@nx/react-native/plugins/jest/svg-mock',
+    },
+}
