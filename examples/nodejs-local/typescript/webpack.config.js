e4c8fbb962f378fb760c4e2c6c40356675da5f88
Adam Wootton <adam@taplytics.com>
2023-08-10 16:02:06 -0400

    fix for __dirname resolution

--- examples/nodejs-local/typescript/webpack.config.js
+++ examples/nodejs-local/typescript/webpack.config.js
@@ -1,6 +1,11 @@
 module.exports = (config) => {
-    console.log(config)
     // config.experiments ??= {}
     // config.experiments.asyncWebAssembly = true
+    config.node = {
+        __dirname: true,
+    }
+    config.context = config.context + '/../../..'
+    console.log(config)
+
     return config
 }
