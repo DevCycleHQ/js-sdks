c66273efcd72d6d9b9605e8d5d6200f376e3e7a9
Adam Wootton <adam@taplytics.com>
2023-10-30 15:40:30 -0400

    type

--- sdk/nextjs/src/server/devcycleServerData.tsx
+++ sdk/nextjs/src/server/devcycleServerData.tsx
@@ -1,16 +1,23 @@
 import { getBucketedConfig } from './bucketing'
 import { getOptions, getSDKKey, getTrackedEvents } from './requestContext'
-import { DevCycleEvent } from '@devcycle/js-client-sdk'
+import { DevCycleEvent, DevCycleUser } from '@devcycle/js-client-sdk'
+import { getUserIdentity } from '@devcycle/next-sdk/server'
 
 export const getDevCycleServerData = async (): Promise<{
     config: Awaited<ReturnType<typeof getBucketedConfig>>
     sdkKey: string
     events: ReturnType<typeof getTrackedEvents>
     options: ReturnType<typeof getOptions>
+    user: DevCycleUser
 }> => {
     const config = await getBucketedConfig()
+    const user = getUserIdentity()
+    if (!user) {
+        throw Error('User should be defined')
+    }
     return {
         config,
+        user,
         sdkKey: getSDKKey(),
         events: getTrackedEvents(),
         options: getOptions(),
