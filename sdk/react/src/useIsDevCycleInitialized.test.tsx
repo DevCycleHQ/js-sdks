7e73851ede9d7ad4e5a852614fe69e3d71092f56
Adam Wootton <adam@taplytics.com>
2023-11-07 11:26:14 -0500

    add tests for init in deferred mode and with boostrap config

--- sdk/react/src/useIsDevCycleInitialized.test.tsx
+++ sdk/react/src/useIsDevCycleInitialized.test.tsx
@@ -1,8 +1,15 @@
 import { act, render, screen } from '@testing-library/react'
 import nock from 'nock'
-import { useIsDevCycleInitialized, withDevCycleProvider } from '.'
+import {
+    DevCycleClient,
+    useDevCycleClient,
+    useIsDevCycleInitialized,
+    withDevCycleProvider,
+} from '.'
 import { mockConfig } from '../mockData/mockConfig'
 import { useState } from 'react'
+import { setTimeout } from '@testing-library/react-native/build/helpers/timers'
+import { BucketedUserConfig } from '@devcycle/types'
 
 jest.unmock('@devcycle/js-client-sdk')
 
@@ -37,6 +44,72 @@ describe('useIsDevCycleInitialized', () => {
 
         expect(await screen.findAllByText('Loading...')).toHaveLength(1)
         await new Promise((resolve) => scope.on('replied', resolve))
+        expect(await screen.findAllByText('Done')).toHaveLength(1)
+    })
+
+    it('should render after identification with deferred initialization', async () => {
+        const scope = nock('https://sdk-api.devcycle.com/v1')
+        scope
+            .defaultReplyHeaders({
+                'access-control-allow-origin': '*',
+            })
+            .get('/sdkConfig')
+            .query((query) => query.user_id === 'test_user')
+            .delay(1000)
+            .reply(200, mockConfig)
+
+        let client: DevCycleClient
+
+        const TestApp = () => {
+            const isReady = useIsDevCycleInitialized()
+            client = useDevCycleClient()
+
+            if (!isReady) {
+                return <div>Loading...</div>
+            }
+
+            return <div>Done</div>
+        }
+
+        const App = withDevCycleProvider({
+            sdkKey: 'dvc_test_key',
+            options: {
+                deferInitialization: true,
+            },
+        })(TestApp)
+
+        render(<App />)
+
+        expect(await screen.findAllByText('Loading...')).toHaveLength(1)
+        await new Promise((resolve) => setTimeout(resolve, 2000))
+        expect(scope.isDone()).toBe(false)
+        act(() => {
+            client.identifyUser({ user_id: 'test_user' })
+        })
+        await new Promise((resolve) => scope.on('replied', resolve))
+        expect(await screen.findAllByText('Done')).toHaveLength(1)
+    })
+
+    it('should render skipping loading state if bootstrapped', async () => {
+        const TestApp = () => {
+            const isReady = useIsDevCycleInitialized()
+
+            if (!isReady) {
+                return <div>Loading...</div>
+            }
+
+            return <div>Done</div>
+        }
+
+        const App = withDevCycleProvider({
+            sdkKey: 'dvc_test_key',
+            options: {
+                bootstrapConfig: {} as BucketedUserConfig,
+            },
+        })(TestApp)
+
+        render(<App />)
+
         expect(await screen.findAllByText('Done')).toHaveLength(1)
     })
 
