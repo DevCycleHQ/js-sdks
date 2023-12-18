eab075888ad6baf5484be1c2ac458f3af538d0b9
Adam Wootton <adam@taplytics.com>
2023-12-18 14:04:24 -0500

    allow update checksums

--- e2e/nextjs/pages-router/app/start.sh
+++ e2e/nextjs/pages-router/app/start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-yarn
+# update checksums to allow any changes to the file-linked packages to update the lockfile in CI without errors
+yarn --update-checksums
 
 # clear cache because next seems to cache node modules even if they change :/
 rm -rf .next
