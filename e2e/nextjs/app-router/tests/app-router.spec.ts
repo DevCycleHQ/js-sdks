6c319693e899da8dbcaf54ca7554d05b08e4ad5e
Adam Wootton <adam@taplytics.com>
2023-12-19 17:11:48 -0500

    fix e2e

--- e2e/nextjs/app-router/tests/app-router.spec.ts
+++ e2e/nextjs/app-router/tests/app-router.spec.ts
@@ -2,7 +2,8 @@ import { test, expect } from '@playwright/test'
 
 test('has expected page elements when streaming mode', async ({ page }) => {
     await page.goto('/?enableStreaming=1')
-    await expect(page.getByText('Streaming Enabled')).toBeVisible()
+    // TODO check for this to be enabled when the option is re-added
+    await expect(page.getByText('Streaming Disabled')).toBeVisible()
 
     await expect(page.getByText('Server Enabled Variable: true')).toBeVisible()
     await expect(
