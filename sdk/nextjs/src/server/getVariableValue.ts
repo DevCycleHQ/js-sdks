37a894b2314fb1365834abc97617dd35a462b1bf
Adam Wootton <adam@taplytics.com>
2023-12-21 10:10:56 -0500

    fix: promise init (#657)

--- sdk/nextjs/src/server/getVariableValue.ts
+++ sdk/nextjs/src/server/getVariableValue.ts
@@ -2,19 +2,35 @@ import { getClient, getInitializedPromise } from './requestContext'
 import { DVCVariableValue } from '@devcycle/js-client-sdk'
 import { VariableTypeAlias } from '@devcycle/types'
 
-export async function getVariableValue<T extends DVCVariableValue>(
-    key: string,
-    defaultValue: T,
-): Promise<VariableTypeAlias<T>> {
-    // wait until next tick to ensure that root layout render has been started and the initialization promise has been
-    // set. Counterintuitively, Next seems to call the render of pages before layouts
-    await new Promise((resolve) => setTimeout(resolve))
-    const initializedPromise = getInitializedPromise()
+/**
+ * keep waiting until next tick to ensure that root layout render has been started and the
+ * initialization promise has been set. Counterintuitively, Next seems to call the render of pages before layouts
+ * Will keep ticking up to 50 times then give up with an error
+ */
+const waitForPromiseAvailable = async () => {
+    let initializedPromise = getInitializedPromise()
+    let attempts = 0
+
+    while (!initializedPromise && attempts < 50) {
+        await new Promise((resolve) => setTimeout(resolve))
+        initializedPromise = getInitializedPromise()
+        attempts++
+    }
+
     if (!initializedPromise) {
         throw new Error(
             'DevCycle server context not found! Did you wrap the root layout in DevCycleServersideProvider?',
         )
     }
+
+    return { initializedPromise }
+}
+
+export async function getVariableValue<T extends DVCVariableValue>(
+    key: string,
+    defaultValue: T,
+): Promise<VariableTypeAlias<T>> {
+    const { initializedPromise } = await waitForPromiseAvailable()
     await initializedPromise
 
     const client = getClient()
