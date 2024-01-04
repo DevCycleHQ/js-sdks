237428f9978170ac007228983b963e166020c7ca
Adam Wootton <adam@taplytics.com>
2024-01-04 09:33:05 -0500

    remove comment

--- e2e/nextjs/app-router/app/app/shared.ts
+++ e2e/nextjs/app-router/app/app/shared.ts
@@ -2,7 +2,6 @@ import { setupDevCycle } from '@devcycle/nextjs-sdk/server'
 const { getVariableValue, getClientContext } = setupDevCycle(
     process.env.NEXT_PUBLIC_E2E_NEXTJS_KEY ?? '',
     async () => {
-        // await new Promise((resolve) => setTimeout(resolve, 5000))
         return {
             user_id: '123',
         }
