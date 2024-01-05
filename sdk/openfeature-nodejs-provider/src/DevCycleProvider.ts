65ce3a5c9282c92e0c3b5fa445060f754f82045a
Jonathan Norris <jonathan@taplytics.com>
2024-01-05 15:34:28 -0500

    chore: replace @openfeature/js-sdk with @openfeature/server-sdk  (#670)
    Signed-off-by: Michael Beemer <beeme1mr@users.noreply.github.com>
    Co-authored-by: Michael Beemer <beeme1mr@users.noreply.github.com>

--- sdk/openfeature-nodejs-provider/src/DevCycleProvider.ts
+++ sdk/openfeature-nodejs-provider/src/DevCycleProvider.ts
@@ -10,7 +10,7 @@ import {
     TargetingKeyMissingError,
     InvalidContextError,
     ProviderStatus,
-} from '@openfeature/js-sdk'
+} from '@openfeature/server-sdk'
 import {
     DevCycleClient,
     DevCycleCloudClient,
