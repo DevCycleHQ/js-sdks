3c4de1134d015eba35172b3961bb5bd4f6553d6a
Jonathan Norris <jonathan@taplytics.com>
2024-01-09 13:45:11 -0500

    fix: cleanup async

--- examples/nextjs/app-router/app/layout.tsx
+++ examples/nextjs/app-router/app/layout.tsx
@@ -11,7 +11,7 @@ export default async function RootLayout({
     return (
         <html lang="en">
             <body>
-                <DevCycleClientsideProvider context={await getClientContext()}>
+                <DevCycleClientsideProvider context={getClientContext()}>
                     {children}
                 </DevCycleClientsideProvider>
             </body>
