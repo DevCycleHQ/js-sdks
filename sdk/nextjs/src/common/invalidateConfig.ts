f62814ce25fac405301219f7c6dd77f777c09690
Adam Wootton <adam@taplytics.com>
2024-01-15 18:10:34 -0500

    fix space

--- sdk/nextjs/src/common/invalidateConfig.ts
+++ sdk/nextjs/src/common/invalidateConfig.ts
@@ -8,7 +8,7 @@ export const invalidateConfig = async (
 ): Promise<void> => {
     if (typeof window != 'undefined') {
         console.error(
-            'DevCycle realtime updates are only available in Next.js 14.0.5 and above. Please update your version' +
+            'DevCycle realtime updates are only available in Next.js 14.0.5 and above. Please update your version ' +
                 'or disable realtime updates.',
         )
         return
