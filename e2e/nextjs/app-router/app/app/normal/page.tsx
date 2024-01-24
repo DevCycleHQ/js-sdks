e74bae9ec1dd207927f79b7775a413f23083568c
Adam Wootton <adam@taplytics.com>
2024-01-24 14:01:08 -0500

    cleanup

--- e2e/nextjs/app-router/app/app/normal/page.tsx
+++ e2e/nextjs/app-router/app/app/normal/page.tsx
@@ -2,17 +2,6 @@ import { ClientComponent } from './ClientComponent'
 import { ServerComponent } from './ServerComponent'
 import React, { Suspense } from 'react'
 import Link from 'next/link'
-import {
-    // ifEnabled,
-    // Test,
-    // DevCycleClientsideProvider,
-    ifEnabled,
-} from '@devcycle/nextjs-sdk'
-
-// console.log('if enabled', ifEnabled)
-// console.log('test', Test)
-// Test()
-// console.log('devcycle', DevCycleClientsideProvider)
 
 const Home = async () => {
     return (
@@ -21,7 +10,6 @@ const Home = async () => {
             <Suspense fallback={<div>Loading...</div>}>
                 <ClientComponent />
                 <ServerComponent />
-                {/*<ConditionalComponent />*/}
             </Suspense>
             <Link href="/normal/test">Go To page</Link>
         </main>
