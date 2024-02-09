febbf7a0fb246d2973b26cc6e2aef37cb2e6db23
Jonathan Norris <jonathan@taplytics.com>
2024-02-09 14:27:09 -0500

    fix: running jest tests with dynamic import using NODE_OPTIONS=--experimental-vm-modules

--- sdk/nodejs/__tests__/initialize.spec.ts
+++ sdk/nodejs/__tests__/initialize.spec.ts
@@ -21,7 +21,6 @@ describe('NodeJS SDK Initialize', () => {
             'dvc_server_token',
         ).getOpenFeatureProvider()
         expect(provider).toBeDefined()
-        expect(provider.status).toBe('NOT_READY')
         await OpenFeature.setProviderAndWait(provider)
         expect(provider.status).toBe('READY')
         const client = OpenFeature.getClient()
