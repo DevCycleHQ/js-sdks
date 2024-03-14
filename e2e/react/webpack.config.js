10eadf3367f27528b55b132248d09bebd4e1ec88
Elliot Campbell <elliotgithub@gmail.com>
2024-03-14 17:01:03 -0400

    create e2e test for built react sdk

---
+++ e2e/react/webpack.config.js
@@ -0,0 +1,70 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const DefinePlugin = require('webpack').DefinePlugin
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+const path = require('path')
+
+module.exports = () => ({
+    entry: './src/main.tsx',
+    mode: 'production',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+        new DefinePlugin({
+            'process.env.NEXT_PUBLIC_E2E_NEXTJS_KEY': JSON.stringify(
+                process.env.NEXT_PUBLIC_E2E_NEXTJS_KEY,
+            ),
+        }),
+        new ForkTsCheckerWebpackPlugin(),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.scss$/i,
+                use: ['sass-to-string', 'sass-loader'],
+            },
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env',
+                            '@babel/preset-react',
+                            '@babel/preset-typescript',
+                        ],
+                    },
+                },
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js', '.tsx'],
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        port: 3000,
+        historyApiFallback: {
+            index: 'index.html',
+        },
+        client: {
+            overlay: {
+                errors: true,
+                warnings: false,
+                runtimeErrors: true,
+            },
+        },
+    },
+    stats: {
+        warnings: false,
+    },
+})
