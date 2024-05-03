7dc94474942958d1db0965bfaab8d6fb529f592d
Jonathan Norris <jonathan@taplytics.com>
2024-05-03 14:47:23 -0400

    feat: add SSEConnection shared lib

---
+++ lib/shared/sse-connection/src/lib/SSEConnection.spec.ts
@@ -0,0 +1,7 @@
+// import { sharedSseconnection } from './SSEConnection'
+//
+// describe('sharedSseconnection', () => {
+//     it('should work', () => {
+//         expect(sharedSseconnection()).toEqual('shared-sse-connection')
+//     })
+// })
---
+++ lib/shared/sse-connection/src/lib/SSEConnection.ts
@@ -0,0 +1,49 @@
+import type { DVCLogger } from '@devcycle/types'
+
+export class SSEConnection {
+    private connection?: EventSource
+
+    constructor(
+        private url: string,
+        private onMessage: (message: unknown) => void,
+        private logger: DVCLogger,
+    ) {
+        this.openConnection()
+    }
+
+    private openConnection() {
+        if (typeof EventSource === 'undefined') {
+            this.logger.warn(
+                'StreamingConnection not opened. EventSource is not available.',
+            )
+            return
+        }
+        this.connection = new EventSource(this.url, { withCredentials: true })
+        this.connection.onmessage = (event) => {
+            this.onMessage(event.data)
+        }
+        this.connection.onerror = () => {
+            this.logger.warn(
+                'StreamingConnection warning. Connection failed to establish.',
+            )
+        }
+        this.connection.onopen = () => {
+            this.logger.debug('StreamingConnection opened')
+        }
+    }
+
+    isConnected(): boolean {
+        return this.connection?.readyState === this.connection?.OPEN
+    }
+
+    reopen(): void {
+        if (!this.isConnected()) {
+            this.close()
+            this.openConnection()
+        }
+    }
+
+    close(): void {
+        this.connection?.close()
+    }
+}
