bd3ac04ec11239d93551d20c5a5f4c5248af8e2e
Jonathan Norris <jonathan@taplytics.com>
2024-05-22 11:18:08 -0400

    feat: add SSEConnection shared lib (#838)

---
+++ lib/shared/sse-connection/src/lib/SSEConnection.spec.ts
@@ -0,0 +1,6 @@
+// Add empty test as a placeholder, passWithNoTests option wasn't working
+describe('SSEConnection', () => {
+    it('empty test', () => {
+        expect(null).toBeNull()
+    })
+})
