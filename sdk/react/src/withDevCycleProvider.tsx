5591b723318fb3c93124c7cc30e1fe79e288e053
Adam Wootton <adam@taplytics.com>
2024-09-13 10:39:26 -0400

    params for initialization

--- sdk/react/src/withDevCycleProvider.tsx
+++ sdk/react/src/withDevCycleProvider.tsx
@@ -9,15 +9,12 @@ import hoistNonReactStatics from 'hoist-non-react-statics'
 import { DevCycleProvider } from './DevCycleProvider'
 import { DVCCustomDataJSON } from '@devcycle/js-client-sdk'
 
-export const withDevCycleProvider =
-    <T extends object, CustomData extends DVCCustomDataJSON = DVCCustomDataJSON>(
-        config: ProviderConfig<CustomData>,
-    ) =>
-    (
+export function withDevCycleProvider<CustomData extends DVCCustomDataJSON>(
+    config: ProviderConfig<CustomData>,
+) {
+    return function <T extends object>(
         WrappedComponent: React.ComponentType<T>,
-    ): ForwardRefExoticComponent<
-        PropsWithoutRef<T> & RefAttributes<unknown>
-    > => {
+    ): ForwardRefExoticComponent<PropsWithoutRef<T> & RefAttributes<unknown>> {
         const HoistedComponent = forwardRef((props: T, ref) => {
             return (
                 <DevCycleProvider config={config}>
@@ -30,6 +27,7 @@ export const withDevCycleProvider =
 
         return HoistedComponent
     }
+}
 
 /**
  * @deprecated Use withDevCycleProvider instead
