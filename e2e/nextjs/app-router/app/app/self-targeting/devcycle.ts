c1b2a1f4a842827dba90ea4d2ee45a958b54d176
Adam Wootton <adam@taplytics.com>
2024-10-07 15:09:27 -0400

    support self targeting in app router

---
+++ e2e/nextjs/app-router/app/app/self-targeting/devcycle.ts
@@ -0,0 +1,22 @@
+import { setupDevCycle } from '@devcycle/nextjs-sdk/server'
+import { headers } from 'next/headers'
+export const {
+    getVariableValue,
+    getClientContext,
+    getAllVariables,
+    getAllFeatures,
+} = setupDevCycle({
+    clientSDKKey: process.env.NEXT_PUBLIC_E2E_NEXTJS_CLIENT_KEY ?? '',
+    serverSDKKey: process.env.E2E_NEXTJS_SERVER_KEY ?? '',
+    userGetter: async () => {
+        const reqHeaders = headers()
+        return {
+            user_id: 'self-targeting-user',
+            customData: {
+                // set a dummy field here so that the headers call stays in the build output
+                someKey: reqHeaders.get('some-key'),
+            },
+        }
+    },
+    options: { enableStreaming: false },
+})
