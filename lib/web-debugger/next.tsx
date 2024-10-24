e8deb3dbe3d06a010160f215597a6e94459e0cd3
Adam Wootton <adam@taplytics.com>
2024-10-24 10:17:40 -0400

    fix debugger build

--- lib/web-debugger/next.tsx
+++ lib/web-debugger/next.tsx
@@ -4,12 +4,18 @@ import {
     DebuggerIframeOptions,
 } from './src/initializeDevCycleDebugger.js'
 import { useDevCycleClient } from '@devcycle/nextjs-sdk'
+import { DevCycleClient } from '@devcycle/js-client-sdk'
 
 export const DevCycleDebugger = (options: DebuggerIframeOptions): null => {
     const client = useDevCycleClient()
 
     useEffect(() => {
-        const cleanupPromise = initializeDevCycleDebugger(client, options)
+        // client here is typed as a DevCycleNextClient which is just a Typescript-omitted version of DevCycleClient
+        // its still a DevCycleClient under the hood, we just want to expose it to Next.js users as having less methods
+        const cleanupPromise = initializeDevCycleDebugger(
+            client as DevCycleClient,
+            options,
+        )
         return () => {
             cleanupPromise.then((cleanup) => cleanup())
         }
