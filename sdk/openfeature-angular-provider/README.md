6541fd1b8bf34fa6c9beb9ebd7411ac74adcad49
Jonathan Norris <jonathan@taplytics.com>
2025-02-13 14:35:56 -0500

    feat: create @devcycle/openfeature-angular-provider

---
+++ sdk/openfeature-angular-provider/README.md
@@ -0,0 +1,15 @@
+# OpenFeature DevCycle Angular Web Provider
+
+This library provides a Javascript implementation of the [OpenFeature](https://openfeature.dev/) Angular Provider interface for [OpenFeature Angular SDK](https://openfeature.dev/docs/reference/technologies/client/web/angular).
+
+## Building
+
+Run `yarn nx build openfeature-angular-provider` to build the library.
+
+## Running Unit Tests
+
+Run `yarn nx test openfeature-angular-provider` to execute the unit tests via [Jest](https://jestjs.io).
+
+## Usage
+
+See our [documentation](https://docs.devcycle.com/sdk/client-side-sdks/angular/angular-openfeature) for more information.
---
+++ sdk/openfeature-angular-provider/jest.config.ts
@@ -0,0 +1,32 @@
+/* eslint-disable */
+export default {
+    displayName: 'openfeature-angular-provider',
+    preset: '../../jest.preset.js',
+    globals: {},
+    transform: {
+        '^.+\\.[tj]s$': [
+            'ts-jest',
+            {
+                tsconfig: '<rootDir>/tsconfig.spec.json',
+            },
+        ],
+    },
+    moduleFileExtensions: ['ts', 'js'],
+    coverageDirectory: '../../coverage/sdk/openfeature-angular-provider',
+    collectCoverage: true,
+    collectCoverageFrom: [
+        '<rootDir>/src/**/*.{ts,js}',
+        '!<rootDir>/**/*.{spec,test,mock}.{ts,js}',
+    ],
+}
+
+module.exports.reporters = [
+    'default',
+    [
+        'jest-junit',
+        {
+            outputDirectory: 'test-results',
+            outputName: `${module.exports.displayName}.xml`,
+        },
+    ],
+]
