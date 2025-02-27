aff65dac32bfe75d7a2366f98242bd3b829659f4
Jonathan Norris <jonathan@taplytics.com>
2025-02-27 11:01:36 -0500

    feat: update the nestJS OF provider to use the new DevCycleProvider to set the sdkPlatform

--- sdk/openfeature-nestjs-provider/src/index.ts
+++ sdk/openfeature-nestjs-provider/src/index.ts
@@ -1,21 +1,26 @@
-import { DevCycleServerSDKOptions, VariableDefinitions } from '@devcycle/types'
 import {
-    initializeDevCycle as originalInitialize,
-    DevCycleClient,
-    DevCycleCloudClient,
+    DevCycleOptionsLocalEnabled,
+    DevCycleOptionsCloudEnabled,
+    DevCycleProvider,
 } from '@devcycle/nodejs-server-sdk'
 
 // Re-export everything from the nodejs-server-sdk
 export * from '@devcycle/nodejs-server-sdk'
 
-/**
- * Initialize DevCycle with NestJS-specific platform settings
- */
-export function initializeDevCycle<
-    Variables extends VariableDefinitions = VariableDefinitions,
->(
-    sdkKey: string,
-    options: DevCycleServerSDKOptions = {},
-): DevCycleClient<Variables> | DevCycleCloudClient<Variables> {
-    return originalInitialize(sdkKey, { ...options, sdkPlatform: 'nestjs-of' })
+export class DevCycleNestJSProvider extends DevCycleProvider {
+    constructor(
+        sdkKey: string,
+        options: DevCycleOptionsLocalEnabled | DevCycleOptionsCloudEnabled = {},
+    ) {
+        const updatedOptions = {
+            ...options,
+            sdkPlatform: 'nestjs-of',
+        }
+
+        if (options.enableCloudBucketing === true) {
+            super(sdkKey, { ...updatedOptions, enableCloudBucketing: true })
+        } else {
+            super(sdkKey, { ...updatedOptions, enableCloudBucketing: false })
+        }
+    }
 }
