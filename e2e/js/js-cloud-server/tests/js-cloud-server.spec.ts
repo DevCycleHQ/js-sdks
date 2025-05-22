962ce77c22d7ab282ae74ee8c6cd4eb9faf16858
Jonathan Norris <jonathan@taplytics.com>
2025-05-22 16:49:49 -0400

    fix: e2e cloud server

--- e2e/js/js-cloud-server/tests/js-cloud-server.spec.ts
+++ e2e/js/js-cloud-server/tests/js-cloud-server.spec.ts
@@ -1,6 +1,6 @@
 describe('js-cloud-server', () => {
     it('should return all variables', async () => {
-        const response = await fetch('http://localhost:63768')
+        const response = await fetch('http://localhost:8787')
         const variablesResponse = await response.json()
 
         expect(variablesResponse).toMatchObject({
