3ba80cecf5540f4b37f76ec2e5dc4d0e6bfacb82
Jonathan Norris <jonathan@taplytics.com>
2025-05-22 14:38:19 -0400

    chore: another lint fix

--- sdk/nextjs/src/client/renderIfEnabled.tsx
+++ sdk/nextjs/src/client/renderIfEnabled.tsx
@@ -2,11 +2,11 @@ import { ComponentProps, ComponentType } from 'react'
 import dynamic from 'next/dynamic'
 import useVariableValue from './useVariableValue'
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 export const renderIfEnabled = <T extends { default: ComponentType<any> }>(
     key: string,
     importFunc: () => Promise<T>,
-): (props: ComponentProps<T['default']>) => JSX.Element | null => {
+) => {
     const Component = dynamic(() => importFunc())
     return function (props: ComponentProps<T['default']>) {
         const isEnabled = useVariableValue(key, false)
