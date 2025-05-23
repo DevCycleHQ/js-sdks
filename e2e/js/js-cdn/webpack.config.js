2308deb3654a42642ab90b69215d1094530bb67d
Jonathan Norris <jonathan@taplytics.com>
2025-05-23 17:14:36 -0400

    chore: cleanup js-cdn e2e changes

--- e2e/js/js-cdn/webpack.config.js
+++ e2e/js/js-cdn/webpack.config.js
@@ -8,18 +8,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 require('dotenv').config({ path: path.resolve(__dirname, '.env') })
 
 module.exports = composePlugins(withNx(), (config, passedArgs) => {
-    // console.log(
-    //    '[DEBUG] Webpack config args (passedArgs):',
-    //    JSON.stringify(passedArgs, null, 2)
-    // );
-
     const options = passedArgs.options
     const context = passedArgs.context
 
-    // console.log('[DEBUG] passedArgs.context:', JSON.stringify(context, null, 2));
-    // console.log('[DEBUG] config.context (from webpack):', config.context);
-
-    config.plugins = config.plugins || [] // Ensure plugins array exists
+    config.plugins = config.plugins || []
 
     config.plugins.push(
         new Dotenv({
@@ -38,54 +30,17 @@ module.exports = composePlugins(withNx(), (config, passedArgs) => {
         }),
     )
 
-    const workspaceRoot = context
-        ? context.workspaceRoot || context.root
-        : undefined
-    // console.log(`[DEBUG] Determined workspaceRoot: ${workspaceRoot}`);
+    const workspaceRoot = context?.workspaceRoot || context?.root
 
-    // Add HtmlWebpackPlugin if options and context are available
-    if (options && options.index && workspaceRoot) {
+    if (options?.index && workspaceRoot) {
         const templatePath = path.resolve(workspaceRoot, options.index)
-        console.log(
-            `[DEBUG] HtmlWebpackPlugin: Using template path: ${templatePath}`,
-        )
         config.plugins.push(
             new HtmlWebpackPlugin({
                 template: templatePath,
-                filename: 'index.html', // Ensure output is index.html
+                filename: 'index.html',
             }),
         )
-    } else {
-        console.error(
-            '[Error] HtmlWebpackPlugin not configured due to missing properties.',
-        )
-        if (!options) {
-            console.error('[Error] passedArgs.options is undefined.')
-        } else {
-            if (!options.index)
-                console.error(
-                    '[Error] passedArgs.options.index is undefined. Value:',
-                    options.index,
-                )
-        }
-        if (!workspaceRoot) {
-            console.error(
-                '[Error] workspaceRoot could not be determined from context.',
-            )
-            if (context) {
-                console.error(
-                    `[DEBUG] context.workspaceRoot: ${context.workspaceRoot}, context.root: ${context.root}`,
-                )
-            } else {
-                console.error('[Error] passedArgs.context is undefined.')
-            }
-        }
     }
-
-    // Removed: config.devServer = config.devServer || {};
-    // Removed commented out devServer modifications as Nx defaults are sufficient.
-
-    // Removed verbose final config log
 
     return config
 })
