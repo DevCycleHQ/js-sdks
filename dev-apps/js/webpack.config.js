bc4c06fc11712899af8609fb4a49fdae196f112f
Jonathan Norris <jonathan@taplytics.com>
2025-05-23 16:50:45 -0400

    chore: run prettier formatting

--- dev-apps/js/webpack.config.js
+++ dev-apps/js/webpack.config.js
@@ -4,16 +4,16 @@ const { composePlugins, withNx } = require('@nx/webpack')
 module.exports = composePlugins(withNx(), (config) => {
     // Note: This was added by an Nx migration. Webpack builds are required to have a corresponding Webpack config file.
     // See: https://nx.dev/recipes/webpack/webpack-config-setup
-    
+
     // Add CSS loader configuration
     config.module = config.module || {}
     config.module.rules = config.module.rules || []
-    
+
     // Add rule for CSS files
     config.module.rules.push({
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
     })
-    
+
     return config
 })
