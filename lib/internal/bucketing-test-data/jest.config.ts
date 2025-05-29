f42ff17ab182b2208ea2fb59e289c1b397ae71fa
Jonathan Norris <jonathan@taplytics.com>
2025-05-29 17:30:14 -0400

    chore: move bucketing-test-data to /lib/internal

--- lib/internal/bucketing-test-data/jest.config.ts
+++ lib/internal/bucketing-test-data/jest.config.ts
@@ -1,6 +1,6 @@
 module.exports = {
   displayName: 'bucketing-test-data',
-  preset: '../jest.preset.js',
+  preset: '../../../jest.preset.js',
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json',
@@ -10,5 +10,5 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../coverage/bucketing-test-data',
+  coverageDirectory: '../../../coverage/lib/internal/bucketing-test-data',
 }
