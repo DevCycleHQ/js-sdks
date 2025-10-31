ed0f36bb1b12dbb4c246bc027e85f5b437118758
phaym <1431593+phaym@users.noreply.github.com>
2025-10-31 15:06:29 -0400

    cleanup logging and conditions

--- sdk/js/src/EventQueue.ts
+++ sdk/js/src/EventQueue.ts
@@ -53,11 +53,7 @@ export class EventQueue<
             )
         }
 
-        console.log(`event flush interval ${eventFlushIntervalMS}`)
-        if (options.next?.disableAutomaticEventFlush === true) {
-            console.warn('event flushing disabled')
-        } else {
-            console.warn('event flushing enabled')
+        if (!options.next?.disableAutomaticEventFlush) {
             this.flushInterval = setInterval(
                 this.flushEvents.bind(this),
                 eventFlushIntervalMS,
@@ -100,9 +96,6 @@ export class EventQueue<
 
         const eventsToFlush = [...this.eventQueue]
         const aggregateEventsToFlush = this.eventsFromAggregateEventMap()
-        console.log(
-            `internal flushing ${this.eventQueue.length} events, ${aggregateEventsToFlush.length} aggEvents`,
-        )
         eventsToFlush.push(...aggregateEventsToFlush)
 
         if (!eventsToFlush.length) {
@@ -161,7 +154,6 @@ export class EventQueue<
             )
             return
         }
-        console.log('queued event')
         this.eventQueue.push(event)
     }
 
@@ -214,7 +206,6 @@ export class EventQueue<
     }
 
     async close(): Promise<void> {
-        console.log('close')
         clearInterval(this.flushInterval)
         await this.flushEvents()
     }
