05136ba147d65c9f47a783dc2c900832b26ac370
phaym <1431593+phaym@users.noreply.github.com>
2025-11-04 17:52:57 -0500

    enable events for next, disable interval server side, after hook to manually flush

--- sdk/js/src/EventQueue.ts
+++ sdk/js/src/EventQueue.ts
@@ -96,6 +96,9 @@ export class EventQueue<
 
         const eventsToFlush = [...this.eventQueue]
         const aggregateEventsToFlush = this.eventsFromAggregateEventMap()
+        console.log(
+            `internal flushing ${this.eventQueue.length} events, ${aggregateEventsToFlush.length} aggEvents`,
+        )
         eventsToFlush.push(...aggregateEventsToFlush)
 
         if (!eventsToFlush.length) {
@@ -154,6 +157,7 @@ export class EventQueue<
             )
             return
         }
+        console.log('queued event')
         this.eventQueue.push(event)
     }
 
@@ -206,6 +210,7 @@ export class EventQueue<
     }
 
     async close(): Promise<void> {
+        console.log('close')
         clearInterval(this.flushInterval)
         await this.flushEvents()
     }
