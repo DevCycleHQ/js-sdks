74ec532df25a20e8bac49881209c3cbf51a4483e
Jonathan Norris <jonathan@taplytics.com>
2025-12-19 16:22:51 -0500

    fix: add fallback for undefined projects in playwright configs

--- e2e/js/js-cdn/playwright.config.ts
+++ e2e/js/js-cdn/playwright.config.ts
@@ -17,7 +17,7 @@ const config = nxE2EPreset(__filename, { testDir: './e2e' })
  */
 export default defineConfig({
     ...config,
-    projects: config.projects.filter((p) => p.name === 'webkit'),
+    projects: config.projects?.filter((p) => p.name === 'webkit') || [{ name: 'webkit', use: { browserName: 'webkit' } }],
     reporter: [['html', { outputFolder: 'playwright-report' }]],
 
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
