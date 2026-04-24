f119ba289e545ff3e15945f29a9f6339eb49f9a5
Jonathan Norris <jonathan.norris@dynatrace.com>
2026-04-24 10:18:26 -0400

    style: run prettier on jest-uuid-cjs.js

--- jest-uuid-cjs.js
+++ jest-uuid-cjs.js
@@ -3,7 +3,13 @@ const { randomUUID } = require('crypto')
 
 module.exports = {
     v4: randomUUID,
-    v1: () => { throw new Error('uuid v1 not implemented in shim') },
-    v3: () => { throw new Error('uuid v3 not implemented in shim') },
-    v5: () => { throw new Error('uuid v5 not implemented in shim') },
+    v1: () => {
+        throw new Error('uuid v1 not implemented in shim')
+    },
+    v3: () => {
+        throw new Error('uuid v3 not implemented in shim')
+    },
+    v5: () => {
+        throw new Error('uuid v5 not implemented in shim')
+    },
 }
